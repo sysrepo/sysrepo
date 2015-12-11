c2f5bd4638c9388d3cc701869c645514344046b6
Rastislav Szabo <raszabo@cisco.com>
2015-12-11 10:17:03 +0100

    NULL check macros, Connection Manager API

--- src/connection_manager.h
+++ src/connection_manager.h
@@ -1,7 +1,7 @@
 /**
  * @file connection_manager.h
  * @author Rastislav Szabo <raszabo@cisco.com>, Lukas Macko <lmacko@cisco.com>
- * @brief 
+ * @brief API of Connection Manager - module that handles all connection to Sysrepo Engine.
  *
  * @copyright
  * Copyright 2015 Cisco Systems, Inc.
@@ -19,12 +19,95 @@
  * limitations under the License.
  */
 
-
 #ifndef SRC_CONNECTION_MANAGER_H_
 #define SRC_CONNECTION_MANAGER_H_
 
+#include "session_manager.h"
 
+/**
+ * @defgroup cm Connection Manager
+ * @{
+ *
+ * @brief Connection Manager is responsible for communication between Sysrepo
+ * daemon (or core engine in library mode) and sysrepo access library
+ * (tha application which is accessing data in sysrepo).
+ *
+ * It handles all connections and does message retreival/delivery from/to
+ * client library.
+ *
+ * It can work in two modes: server (daemon), or local (library) mode. See
+ * ::cm_start_server and ::cm_start_local for more information.
+ */
 
+/**
+ * @brief Connection Manager context used to identify particular instance of
+ * Connection Manager.
+ */
+typedef struct cm_ctx_s cm_ctx_t;
 
+/**
+ * @brief Initializes Connection Manager in server (daemon) mode.
+ *
+ * After initialization, clients (other applications) are able to connect
+ * to the server (as of now, by connecting to server's unix-domain socket).
+ *
+ * This function will block the thread in the event loop until stop is requested
+ * or until an error occurs.
+ *
+ * @param[in] socket_path Path to the unix-domain socket where server should bind to.
+ * @param[out] cm_ctx Connectaion manager context which can be used in subsequent
+ * CM API calls.
+ *
+ * @return Error code (SR_ERR_OK on success).
+ */
+int cm_start_server(const char *socket_path, cm_ctx_t **cm_ctx);
+
+/**
+ * @brief Initializes Connection Manager in local (library) mode.
+ *
+ * The function will initialize an socket pair and return a socket file descriptor
+ * that can be used for client communication with Connection Manager. No other
+ * clients will be able to connect to this instance of Connection Manager.
+ *
+ * Connection Manager will be initialized and run in a new thread, so this
+ * function call won't block more then needed to execute a new thread. The thread
+ * will be automatically later stopped and destroyed by ::cm_cleanup call.
+ *
+ * @param[out] cm_ctx Connectaion Manager context which can be used in subsequent
+ * CM API calls.
+ * @param[out] communication_fd Socket file descriptor that can be used for
+ * client communication with Connection Manager.
+ *
+ * @return Error code (SR_ERR_OK on success).
+ */
+int cm_start_local(cm_ctx_t **cm_ctx, int *communication_fd);
+
+/**
+ * @brief Cleans up a Connection Manager instance.
+ *
+ * All open connections will be closed and all memory held by CM will be released.
+ * In case of local CM mode, cleanup will cause immediate destroy of local
+ * CM instance and can be called at any time. In case of server CM mode, this
+ * can be called only after ::cm_start_server returns.
+ *
+ * @param[in] cm_ctx Connection Manager context to be released.
+ *
+ * @return Error code (SR_ERR_OK on success).
+ */
+int cm_cleanup(const cm_ctx_t *cm_ctx);
+
+/**
+ * @brief Sends the message to the proper reciepient according to provided session.
+ *
+ * This function is thread safe, can be called from any thread.
+ *
+ * @param[in] session Session context used to identifiy the receiver.
+ * @param[in] msg Messge to be send.
+ *
+ * @return Error code (SR_ERR_OK on success).
+ */
+int cm_msg_send(sm_session_t session, void *msg);
+
+/**@} cm */
 
 #endif /* SRC_CONNECTION_MANAGER_H_ */
