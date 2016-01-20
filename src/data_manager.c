95c2ce2f2e846828b09e4cd3baa66a2bb3f5d628
Lukas Macko <lmacko@cisco.com>
2016-01-20 09:04:41 +0100

    dm: avl_locking

--- src/data_manager.c
+++ src/data_manager.c
@@ -32,6 +32,7 @@ typedef struct dm_ctx_s {
     struct ly_ctx *ly_ctx;
     pthread_mutex_t lyctx_lock;
     avl_tree_t *module_avl;
+    pthread_rwlock_t avl_lock;
 } dm_ctx_t;
 
 typedef struct dm_session_s {
@@ -207,10 +208,12 @@ dm_add_empty_data_tree(const dm_ctx_t *dm_ctx, const struct lys_module *module, 
  * @return err_code
  */
 static int
-dm_find_module_schema(const dm_ctx_t *dm_ctx, const char *module_name, const struct lys_module **module)
+dm_find_module_schema(dm_ctx_t *dm_ctx, const char *module_name, const struct lys_module **module)
 {
     CHECK_NULL_ARG2(dm_ctx, module_name);
+    pthread_mutex_lock(&dm_ctx->lyctx_lock);
     *module = ly_ctx_get_module(dm_ctx->ly_ctx, module_name, NULL);
+    pthread_mutex_unlock(&dm_ctx->lyctx_lock);
     return *module == NULL ? SR_ERR_UNKNOWN_MODEL : SR_ERR_OK;
 }
 
@@ -239,17 +242,18 @@ dm_load_data_tree(dm_ctx_t *dm_ctx, const struct lys_module *module, struct lyd_
     if (NULL != f) {
         pthread_mutex_lock(&dm_ctx->lyctx_lock);
         *data_tree = lyd_parse_fd(dm_ctx->ly_ctx, fileno(f), LYD_XML, LYD_OPT_STRICT);
-        pthread_mutex_unlock(&dm_ctx->lyctx_lock);
         if (NULL == *data_tree) {
             SR_LOG_ERR("Parsing data tree from file %s failed", data_filename);
             free(data_filename);
             fclose(f);
+            pthread_mutex_unlock(&dm_ctx->lyctx_lock);
             return SR_ERR_INTERNAL;
         }
         fclose(f);
     } else {
         SR_LOG_INF("File %s couldn't be opened for reading, file probably doesn't exist", data_filename);
         free(data_filename);
+        pthread_mutex_unlock(&dm_ctx->lyctx_lock);
         return SR_ERR_NOT_FOUND;
     }
 
@@ -257,19 +261,24 @@ dm_load_data_tree(dm_ctx_t *dm_ctx, const struct lys_module *module, struct lyd_
         SR_LOG_ERR("Loaded data tree '%s' is not valid", data_filename);
         free(data_filename);
         sr_free_datatree(*data_tree);
+        pthread_mutex_unlock(&dm_ctx->lyctx_lock);
         return SR_ERR_INTERNAL;
     }
+    pthread_mutex_unlock(&dm_ctx->lyctx_lock);
 
     SR_LOG_INF("Data file %s loaded successfuly", data_filename);
     free(data_filename);
 
+    pthread_rwlock_wrlock(&dm_ctx->avl_lock);
     avl_node_t *avl_node = avl_insert(dm_ctx->module_avl, *data_tree);
     if (NULL == avl_node) {
         SR_LOG_ERR("Insert data tree %s into avl tree failed", module->name);
         sr_free_datatree(*data_tree);
         *data_tree = NULL;
+        pthread_rwlock_unlock(&dm_ctx->avl_lock);
         return SR_ERR_INTERNAL;
     }
+    pthread_rwlock_unlock(&dm_ctx->avl_lock);
 
     return SR_ERR_OK;
 }
@@ -292,12 +301,15 @@ dm_find_data_tree(dm_ctx_t *dm_ctx, const struct lys_module *module, struct lyd_
     }
 
     /* look up in loaded */
+    pthread_rwlock_rdlock(&dm_ctx->avl_lock);
     avl_node = avl_search(dm_ctx->module_avl, data_node);
     lyd_free(data_node);
     if (NULL != avl_node) {
         *data_tree = avl_node->item;
+        pthread_rwlock_unlock(&dm_ctx->avl_lock);
         return SR_ERR_OK;
     }
+    pthread_rwlock_unlock(&dm_ctx->avl_lock);
 
     /* try to load data_tree */
     rc = dm_load_data_tree(dm_ctx, module, data_tree);
@@ -349,6 +361,16 @@ dm_init(const char *search_dir, dm_ctx_t **dm_ctx)
         dm_cleanup(ctx);
         return SR_ERR_INTERNAL;
     }
+    
+    pthread_rwlockattr_t    attr;
+    pthread_rwlockattr_init(&attr);
+    pthread_rwlockattr_setkind_np(&attr, PTHREAD_RWLOCK_PREFER_WRITER_NONRECURSIVE_NP);
+
+    if (0 != pthread_rwlock_init(&ctx->avl_lock, &attr)){
+        SR_LOG_ERR_MSG("avl rwlock init failed");
+        dm_cleanup(ctx);
+        return SR_ERR_INTERNAL;
+    }
 
     *dm_ctx = ctx;
     int res = dm_load_schemas(ctx);
@@ -368,6 +390,7 @@ dm_cleanup(dm_ctx_t *dm_ctx)
     avl_free_tree(dm_ctx->module_avl);
     ly_ctx_destroy(dm_ctx->ly_ctx);
     free(dm_ctx);
+    pthread_rwlock_destroy(&dm_ctx->avl_lock);
     return SR_ERR_OK;
 }
 
