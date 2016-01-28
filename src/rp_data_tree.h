276fb0d81f9bddceaba02a9c673bfb639c9a119b
Lukas Macko <lmacko@cisco.com>
2016-01-28 14:04:04 +0100

    rp_dt comments

--- src/rp_data_tree.h
+++ src/rp_data_tree.h
@@ -189,7 +189,7 @@ int rp_dt_get_values_wrapper_with_opts(dm_ctx_t *dm_ctx, dm_session_t *dm_sessio
 
 
 /**
- * @brief Deletes item(s) identified by xpath
+ * @brief Deletes item(s) identified by xpath. List key can not be deleted.
  * @param [in] dm_ctx
  * @param [in] session
  * @param [in] datastore
@@ -200,14 +200,15 @@ int rp_dt_get_values_wrapper_with_opts(dm_ctx_t *dm_ctx, dm_session_t *dm_sessio
 int rp_dt_delete_item(dm_ctx_t *dm_ctx, dm_session_t *session, const sr_datastore_t datastore, const char *xpath, const sr_edit_flag_t options);
 
 /**
- * @brief Creates a leaf or leaf list node
+ * @brief Function can create presence container, list instance, leaf, leaf-list item. If the xpath identifies leaf-list value is appended to the end
+ * of the leaf-list. Value of the list key can not be set or changed.
  * @param [in] dm_ctx
  * @param [in] session
  * @param [in] datastore
  * @param [in] xpath
  * @param [in] options
- * @param [in] value
- * @return err_code
+ * @param [in] value the value to be set (xpath inside the structure is ignored), in case of presence container or list instance is ignored can be NULL 
+ * @return err_code SR_ERR_OK on success
  */
 int rp_dt_set_item(dm_ctx_t *dm_ctx, dm_session_t *session, const sr_datastore_t datastore, const char *xpath, const sr_edit_flag_t options, const sr_val_t *value);
 
