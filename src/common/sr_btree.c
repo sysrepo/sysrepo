48667d07fdcb8e57317eaff22874ab332d001f9f
Rastislav Szabo <raszabo@cisco.com>
2016-02-08 14:41:53 +0100

    sr_btree documentation

--- src/common/sr_btree.c
+++ src/common/sr_btree.c
@@ -22,24 +22,30 @@
 #include "sr_common.h"
 #include "sr_btree.h"
 
-#if defined(USE_AVL_LIB)
+#ifdef USE_AVL_LIB
 #include <avl.h>
 #else
 #include <redblack.h>
 #endif
 
+/**
+ * @brief Common context of balanced binary tree, independent of the library used.
+ */
 typedef struct sr_btree_s {
-#if defined(USE_AVL_LIB)
-    avl_tree_t *avl_tree;
+#ifdef USE_AVL_LIB
+    avl_tree_t *avl_tree;    /**< AVL tree context. */
 #else
-    struct rbtree *rb_tree;
-    RBLIST *rb_list;
+    struct rbtree *rb_tree;  /**< Red-black tree context. */
+    RBLIST *rb_list;         /**< List used to walk in the red-black tree. */
 #endif
     sr_btree_compare_item_cb compare_item_cb;
     sr_btree_free_item_cb free_item_cb;
 } sr_btree_t;
 
-#if !defined(USE_AVL_LIB)
+#ifndef USE_AVL_LIB
+/**
+ * @brief internal callback used only by Red-black tree.
+ */
 static int
 sr_redblack_compare_item_cb(const void *item1, const void *item2, const void *ctx)
 {
@@ -65,7 +71,7 @@ sr_btree_init(sr_btree_compare_item_cb compare_item_cb, sr_btree_free_item_cb fr
     tree->compare_item_cb = compare_item_cb;
     tree->free_item_cb = free_item_cb;
 
-#if defined(USE_AVL_LIB)
+#ifdef USE_AVL_LIB
     tree->avl_tree = avl_alloc_tree(compare_item_cb, free_item_cb);
     if (NULL == tree->avl_tree) {
         return SR_ERR_NOMEM;
@@ -85,7 +91,7 @@ void
 sr_btree_cleanup(sr_btree_t* tree)
 {
     if (NULL != tree) {
-#if defined(USE_AVL_LIB)
+#ifdef USE_AVL_LIB
         /* calls free item callback on each node & destroys the tree  */
         avl_free_tree(tree->avl_tree);
 #else
@@ -111,7 +117,7 @@ sr_btree_insert(sr_btree_t *tree, void *item)
 {
     CHECK_NULL_ARG2(tree, item);
 
-#if defined(USE_AVL_LIB)
+#ifdef USE_AVL_LIB
     avl_node_t *node = avl_insert(tree->avl_tree, item);
     if (NULL == node) {
         if (EEXIST == errno) {
@@ -137,7 +143,7 @@ sr_btree_delete(sr_btree_t *tree, void *item)
 {
     CHECK_NULL_ARG_VOID2(tree, item);
 
-#if defined(USE_AVL_LIB)
+#ifdef USE_AVL_LIB
     avl_delete(tree->avl_tree, item);
 #else
     rbdelete(item, tree->rb_tree);
@@ -152,7 +158,7 @@ sr_btree_search(const sr_btree_t *tree, const void *item)
         return NULL;
     }
 
-#if defined(USE_AVL_LIB)
+#ifdef USE_AVL_LIB
     avl_node_t *node = avl_search(tree->avl_tree, item);
     if (NULL != node) {
         return node->item;
@@ -171,7 +177,7 @@ sr_btree_get_at(sr_btree_t *tree, size_t index)
         return NULL;
     }
 
-#if defined(USE_AVL_LIB)
+#ifdef USE_AVL_LIB
     avl_node_t *node = avl_at(tree->avl_tree, index);
     if (NULL != node) {
         return node->item;
