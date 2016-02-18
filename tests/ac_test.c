1f564e62ec6354574653ce8494a75aa941a97b4b
Rastislav Szabo <raszabo@cisco.com>
2016-02-18 10:33:58 +0100

    AC tests for sudo execution

--- tests/ac_test.c
+++ tests/ac_test.c
@@ -31,18 +31,23 @@
 #include "test_module_helper.h"
 
 static int
-logging_setup(void **state)
+ac_test_setup(void **state)
 {
     sr_set_log_level(SR_LL_DBG, SR_LL_ERR); /* print debugs to stderr */
     sr_logger_init("ac_test");
+
+    unlink(TEST_MODULE_DATA_FILE_NAME);
+    createDataTreeTestModule();
 
     return 0;
 }
 
 static int
-logging_cleanup(void **state)
+ac_test_teardown(void **state)
 {
     sr_logger_cleanup();
+
+    unlink(TEST_MODULE_DATA_FILE_NAME);
 
     return 0;
 }
@@ -76,15 +81,15 @@ ac_test_unpriviledged(void **state)
     rc = xp_char_to_loc_id(XP_TEST_MODULE_STRING, &loc_id);
     assert_int_equal(rc, SR_ERR_OK);
 
+    /* attempt 1 */
     rc = ac_check_node_permissions(session, loc_id, AC_OPER_READ);
     assert_int_equal(rc, SR_ERR_OK);
-
     rc = ac_check_node_permissions(session, loc_id, AC_OPER_READ_WRITE);
     assert_int_equal(rc, SR_ERR_OK);
 
+    /* attempt 2 */
     rc = ac_check_node_permissions(session, loc_id, AC_OPER_READ);
     assert_int_equal(rc, SR_ERR_OK);
-
     rc = ac_check_node_permissions(session, loc_id, AC_OPER_READ_WRITE);
     assert_int_equal(rc, SR_ERR_OK);
 
@@ -106,7 +111,7 @@ static void
 ac_test_priviledged(void **state)
 {
     ac_ctx_t *ctx = NULL;
-    ac_session_t *session = NULL;
+    ac_session_t *session1 = NULL, *session2 = NULL;
     xp_loc_id_t *loc_id = NULL;
     int rc = SR_ERR_OK;
 
@@ -127,50 +132,59 @@ ac_test_priviledged(void **state)
     credentials2.r_uid = getuid();
     credentials2.r_gid = getgid();
     credentials2.e_username = getenv("SUDO_USER");
-    credentials2.e_uid = getenv("SUDO_UID");
-    credentials2.e_gid = getenv("SUDO_GID");
+    credentials2.e_uid = atoi(getenv("SUDO_UID"));
+    credentials2.e_gid = atoi(getenv("SUDO_GID"));
 
     /* init */
     rc = ac_init(&ctx);
     assert_int_equal(rc, SR_ERR_OK);
-    rc = ac_session_init(ctx, &credentials1, &session);
+    rc = ac_session_init(ctx, &credentials1, &session1);
+    assert_int_equal(rc, SR_ERR_OK);
+    rc = ac_session_init(ctx, &credentials2, &session2);
     assert_int_equal(rc, SR_ERR_OK);
 
     /* node permission checks */
     rc = xp_char_to_loc_id(XP_TEST_MODULE_STRING, &loc_id);
     assert_int_equal(rc, SR_ERR_OK);
 
-    rc = ac_check_node_permissions(session, loc_id, AC_OPER_READ);
+    /* credentials 1 */
+    rc = ac_check_node_permissions(session1, loc_id, AC_OPER_READ);
+    assert_int_equal(rc, SR_ERR_OK);
+    rc = ac_check_node_permissions(session1, loc_id, AC_OPER_READ_WRITE);
     assert_int_equal(rc, SR_ERR_OK);
 
-    rc = ac_check_node_permissions(session, loc_id, AC_OPER_READ_WRITE);
+    /* credentials 2 */
+    rc = ac_check_node_permissions(session2, loc_id, AC_OPER_READ);
     assert_int_equal(rc, SR_ERR_OK);
-
-    rc = ac_check_node_permissions(session, loc_id, AC_OPER_READ);
-    assert_int_equal(rc, SR_ERR_OK);
-
-    rc = ac_check_node_permissions(session, loc_id, AC_OPER_READ_WRITE);
-    assert_int_equal(rc, SR_ERR_OK);
+    rc = ac_check_node_permissions(session2, loc_id, AC_OPER_READ_WRITE);
+    assert_int_equal(rc, SR_ERR_UNAUTHORIZED);
 
     xp_free_loc_id(loc_id);
 
     /* file permission checks */
-    rc = ac_check_file_permissions(session, "/etc/passwd", AC_OPER_READ);
+
+    /* credentials 1 */
+    rc = ac_check_file_permissions(session1, "/etc/passwd", AC_OPER_READ);
+    assert_int_equal(rc, SR_ERR_OK);
+    rc = ac_check_file_permissions(session1, "/etc/passwd", AC_OPER_READ_WRITE);
     assert_int_equal(rc, SR_ERR_OK);
 
-    rc = ac_check_file_permissions(session, "/etc/passwd", AC_OPER_READ_WRITE);
+    /* credentials 2 */
+    rc = ac_check_file_permissions(session2, "/etc/passwd", AC_OPER_READ);
     assert_int_equal(rc, SR_ERR_OK);
+    rc = ac_check_file_permissions(session2, "/etc/passwd", AC_OPER_READ_WRITE);
+    assert_int_equal(rc, SR_ERR_UNAUTHORIZED);
 
     /* cleanup */
-    ac_session_cleanup(session);
+    ac_session_cleanup(session1);
     ac_cleanup(ctx);
 }
 
 int
 main() {
     const struct CMUnitTest tests[] = {
-            cmocka_unit_test_setup_teardown(ac_test_unpriviledged, logging_setup, logging_cleanup),
-            cmocka_unit_test_setup_teardown(ac_test_priviledged, logging_setup, logging_cleanup),
+            cmocka_unit_test_setup_teardown(ac_test_unpriviledged, ac_test_setup, ac_test_teardown),
+            cmocka_unit_test_setup_teardown(ac_test_priviledged, ac_test_setup, ac_test_teardown),
     };
 
     return cmocka_run_group_tests(tests, NULL, NULL);
