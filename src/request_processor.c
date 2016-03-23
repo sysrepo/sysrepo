e269ff4d1d633894308bb60f2205a2e24f141f29
Lukas Macko <lmacko@cisco.com>
2016-03-23 08:25:03 +0100

    Rp fix SA

--- src/request_processor.c
+++ src/request_processor.c
@@ -583,7 +583,7 @@ rp_commit_req_process(rp_ctx_t *rp_ctx, rp_session_t *session, Sr__Msg *msg)
     resp->response->result = rc;
 
     /* copy error information to GPB  (if any) */
-    if (NULL != errors) {
+    if (err_cnt > 0) {
         sr_gpb_fill_errors(errors, err_cnt, &resp->response->commit_resp->errors, &resp->response->commit_resp->n_errors);
         sr_free_errors(errors, err_cnt);
     }
@@ -659,7 +659,7 @@ rp_session_refresh_req_process(rp_ctx_t *rp_ctx, rp_session_t *session, Sr__Msg 
     resp->response->result = rc;
 
     /* copy error information to GPB  (if any) */
-    if (err_cnt > 0) {
+    if (NULL != errors) {
         sr_gpb_fill_errors(errors, err_cnt, &resp->response->session_refresh_resp->errors, &resp->response->session_refresh_resp->n_errors);
         sr_free_errors(errors, err_cnt);
     }
