b36791034dead192efc17f2bbd6694c0e0001cda
Lukas Macko <lmacko@cisco.com>
2016-04-01 08:07:03 +0200

    Enable test and fix libyang choice change

--- tests/rp_dt_edit_test.c
+++ tests/rp_dt_edit_test.c
@@ -1008,7 +1008,6 @@ edit_validate_test(void **state)
     test_rp_session_cleanup(ctx, session);
 
     /* choice */
-/**WAITING FOR LIBYANG FIX
     test_rp_sesssion_create(ctx, SR_DS_STARTUP, &session);
     sr_val_t interval;
     interval.xpath = NULL;
@@ -1035,12 +1034,13 @@ edit_validate_test(void **state)
     errors = NULL;
     e_cnt = 0;
 
+    /* Validation should pass because libyang automatically overwrites nodes from different choice alternative */
     rc = dm_validate_session_data_trees(ctx->dm_ctx, session->dm_session, &errors, &e_cnt);
-    assert_int_equal(SR_ERR_VALIDATION_FAILED, rc);
+    assert_int_equal(SR_ERR_OK, rc);
     sr_free_errors(errors, e_cnt);
 
     test_rp_session_cleanup(ctx, session);
-*/
+
     /* leaf-list unique values */
     test_rp_sesssion_create(ctx, SR_DS_STARTUP, &session);
     sr_val_t val;
