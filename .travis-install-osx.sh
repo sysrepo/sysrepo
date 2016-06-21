071e0dd39acdb4b29eff1478ff877101440fabf1
Lukas Macko <lmacko@cisco.com>
2016-06-21 14:32:50 +0200

    Update .travis

--- .travis-install-osx.sh
+++ .travis-install-osx.sh
@@ -4,17 +4,17 @@ brew install libev
 brew install pcre
 
 # CMocka
-    git clone git://git.cryptomilk.org/projects/cmocka.git
-    cd cmocka ; mkdir build; cd build
-    cmake ..
-    make -j2 && make install
-    cd ../..
+git clone git://git.cryptomilk.org/projects/cmocka.git
+cd cmocka ; mkdir build; cd build
+cmake ..
+make -j2 && make install
+cd ../..
 
-    git clone https://github.com/CESNET/libyang.git
-    cd libyang ; mkdir build ; cd build
-    cmake -DCMAKE_BUILD_TYPE=Debug -DENABLE_BUILD_TESTS=OFF ..
-    make -j2 && sudo make install
-    cd ../..
+git clone https://github.com/CESNET/libyang.git
+cd libyang ; mkdir build ; cd build
+cmake -DCMAKE_BUILD_TYPE=Debug -DENABLE_BUILD_TESTS=OFF ..
+make -j2 && sudo make install
+cd ../..
 
 #libredblack
 git clone https://github.com/sysrepo/libredblack.git
