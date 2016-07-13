010c017672cd1702bcb5cf64491475c05bf8a372
Lukas Macko <lmacko@cisco.com>
2016-07-13 14:35:02 +0200

    test CONFIG_ONLY session doesn't return state data

--- tests/cl_test.c
+++ tests/cl_test.c
@@ -1913,7 +1913,7 @@ cl_dp_get_items_test(void **state)
 {
     sr_conn_ctx_t *conn = *state;
     assert_non_null(conn);
-    sr_session_ctx_t *session = NULL;
+    sr_session_ctx_t *session = NULL, *config_only_session = NULL;
     sr_subscription_ctx_t *subscription = NULL;
     int rc = SR_ERR_OK;
     sr_val_t *value = NULL;
@@ -1934,12 +1934,21 @@ cl_dp_get_items_test(void **state)
     assert_int_equal(42, value->data.uint32_val);
     sr_free_val(value);
 
+    rc = sr_session_start(conn, SR_DS_RUNNING, SR_SESS_CONFIG_ONLY, &config_only_session);
+    assert_int_equal(rc, SR_ERR_OK);
+
+    rc = sr_get_item(config_only_session, "/state-module:bus/distance_travelled", &value);
+    assert_int_equal(rc, SR_ERR_NOT_FOUND);
+
     /* unsubscribe */
     rc = sr_unsubscribe(NULL, subscription);
     assert_int_equal(rc, SR_ERR_OK);
 
     /* stop the session */
     rc = sr_session_stop(session);
+    assert_int_equal(rc, SR_ERR_OK);
+
+    rc = sr_session_stop(config_only_session);
     assert_int_equal(rc, SR_ERR_OK);
 }
 
