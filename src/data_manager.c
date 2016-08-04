2bd71cd8b24857255a0f29de85c3aea3aaf66c38
Lukas Macko <lmacko@cisco.com>
2016-08-04 14:58:01 +0200

    Get changes handle edit of default nodes as modified + tests

--- src/data_manager.c
+++ src/data_manager.c
@@ -2801,9 +2801,8 @@ dm_commit_notify(dm_ctx_t *dm_ctx, dm_session_t *session, dm_commit_context_t *c
             continue;
         }
 
-        lyd_wd_cleanup(&prev_info->node, 0);
-        struct lyd_difflist *diff = lyd_diff(prev_info->node, commit_info->node, 0);
         lyd_wd_add(commit_info->schema->ly_ctx, &commit_info->node, LYD_WD_IMPL_TAG);
+        struct lyd_difflist *diff = lyd_diff(prev_info->node, commit_info->node, 0);
         if (NULL == diff) {
             SR_LOG_ERR("Lyd diff failed for module %s", info->schema->module->name);
             continue;
