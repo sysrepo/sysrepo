1967b1b55510d2ffbf7a8c8eeceedfd88588516c
Mislav Novakovic <mislav.novakovic@sartura.hr>
2016-10-05 15:19:55 +0200

    python2: add rpc_sen_tree function to rpc_example.py
    Signed-off-by: Mislav Novakovic <mislav.novakovic@sartura.hr>

--- swig/python2/examples/rpc_example.py
+++ swig/python2/examples/rpc_example.py
@@ -80,6 +80,28 @@ def test_rpc_cb(xpath, in_vals, holder, private_ctx):
     except Exception as e:
         print e
 
+def test_rpc_tree_cb(xpath, in_trees, holder, private_ctx):
+    try:
+        print "\n ========== RPC TREE CALLED ==========\n"
+
+        out_trees = holder.allocate(3)
+
+        if (in_trees == None or out_trees == None):
+            return
+
+        for n in range(in_trees.tree_cnt()):
+            print in_trees.tree(n).name() + " = " + in_trees.tree(n).data().get_string()
+
+        out_trees.tree(0).set_name("status")
+        out_trees.tree(0).set("The image acmefw-2.3 is being installed.", sr.SR_STRING_T)
+        out_trees.tree(1).set_name("version")
+        out_trees.tree(1).set("2.3", sr.SR_STRING_T)
+        out_trees.tree(2).set_name("location")
+        out_trees.tree(2).set("/root/", sr.SR_STRING_T)
+
+    except Exception as e:
+        print e
+
 try:
     module_name = "test-module"
 
@@ -103,13 +125,31 @@ try:
 
 
     print "\n ========== START RPC CALL =========="
-    out_vals = subscribe.rpc_send("/test-module:activate-software-image", in_vals);
+    out_vals = subscribe.rpc_send("/test-module:activate-software-image", in_vals)
 
     print "\n ========== PRINT RETURN VALUE =========="
     for n in range (out_vals.val_cnt()):
         print_value(out_vals.val(n))
 
-    print "\n ========== END PROGRAM =========="
+    print "\n ========== SUBSCRIBE TO RPC TREE CALL ==========\n";
+    subscribe.rpc_subscribe_tree("/test-module:activate-software-image", test_rpc_tree_cb)
+
+    in_trees = sr.Trees(1)
+
+    in_trees.tree(0).set_name("image-name");
+    in_trees.tree(0).set("acmefw-2.3", sr.SR_STRING_T);
+
+    print "\n ========== START RPC TREE CALL ==========\n"
+    out_trees = subscribe.rpc_send_tree("/test-module:activate-software-image", in_trees)
+
+    print "\n ========== PRINT RETURN VALUE ==========\n"
+    for n in range (out_trees.tree_cnt()):
+        if (out_trees.tree(n).type() == sr.SR_STRING_T):
+            print out_trees.tree(n).name() + " = " + out_trees.tree(n).data().get_string()
+        if (out_trees.tree(n).type() == sr.SR_CONTAINER_T):
+            print out_trees.tree(n).name() + " = (container)"
+
+    print "\n ========== END PROGRAM ==========\n"
 
 except Exception as e:
     print e
