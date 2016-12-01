3e6f74e5585d8c4ada01f4cb8371a24478eba721
Rastislav Szabo <raszabo@cisco.com>
2016-12-01 08:44:47 +0100

    dbg. travis

--- deploy/travis/report-crash.sh
+++ deploy/travis/report-crash.sh
@@ -6,7 +6,8 @@ COREFILES=$(find . -name "core*")
 for CORE in ${COREFILES}
 do
     echo -e "\n\n>>> Core file '${CORE}' was found: "
+    cat "${CORE}"
     EXE=$(file $CORE | sed -n "s/^.*, from '\(.\+\)'$/\1/p")
     gdb -ex "core ${CORE}" -ex "thread apply all bt" -batch --args ${EXE}
-    cat ${CORE}
+    gdb -ex "core ${CORE}" -ex "thread apply all bt" -batch --args sysrepoctl
 done
