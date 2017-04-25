411a46d53c91dde378aa608ce154456d3f71fe0d
Antonio Paunovic <antonio.paunovic@sartura.hr>
2017-04-25 12:20:02 +0000

    Fix TOCTOU bug from coverity.
    Signed-off-by: Antonio Paunovic <antonio.paunovic@sartura.hr>

--- src/executables/sysrepoctl.c
+++ src/executables/sysrepoctl.c
@@ -903,14 +903,11 @@ srctl_install(const char *yang, const char *yin, const char *owner, const char *
     }
 
     /* make sure that target locations are available */
-    if (access(srctl_schema_search_dir, F_OK) == -1) {
-        /* create schema repository */
-        ret = mkdir(srctl_schema_search_dir, S_IRWXU | S_IRWXG | S_IRWXO);
-        if (ret == -1) {
-            fprintf(stderr, "Error: Unable to create schema repository '%s': %s.\n", srctl_schema_search_dir,
-                    sr_strerror_safe(errno));
-            goto fail;
-        }
+    ret = mkdir(srctl_schema_search_dir, S_IRWXU | S_IRWXG | S_IRWXO);
+    if (ret == -1 && errno != EEXIST) {
+        fprintf(stderr, "Error: Unable to create schema repository '%s': %s.\n", srctl_schema_search_dir,
+                sr_strerror_safe(errno));
+        goto fail;
     }
     if (access(srctl_schema_search_dir, W_OK) == -1) {
         fprintf(stderr, "Error: Unable to write into the schema repository '%s': %s.\n", srctl_schema_search_dir,
