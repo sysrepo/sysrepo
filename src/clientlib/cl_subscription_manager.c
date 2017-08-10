2a6978c79f069218747325cae1e2faced8c52644
Mislav Novakovic <mislav.novakovic@sartura.hr>
2017-08-10 15:39:26 +0200

    BUGFIX for missing mkstemps in uClibc
    Signed-off-by: Mislav Novakovic <mislav.novakovic@sartura.hr>

--- src/clientlib/cl_subscription_manager.c
+++ src/clientlib/cl_subscription_manager.c
@@ -31,6 +31,9 @@
 #include <pthread.h>
 #include <arpa/inet.h>
 #include <ev.h>
+#ifndef HAVE_MKSTEMPS
+#include <fcntl.h>
+#endif
 
 #include "cl_subscription_manager.h"
 #include "sr_common.h"
@@ -1447,13 +1450,26 @@ cl_sm_get_server_socket_filename(cl_sm_ctx_t *sm_ctx, const char *module_name, c
     snprintf(pid_str, 20, "%d", getpid());
     strncat(path, pid_str, PATH_MAX - strlen(path) - 1);
 
+#ifdef HAVE_MKSTEMPS
     /* append temporary file name part */
     strncat(path, ".XXXXXX.sock", PATH_MAX - strlen(path) - 1);
     fd = mkstemps(path, 5);
-    if (-1 != fd) {
+    if (-1 == fd) {
         close(fd);
         unlink(path);
     }
+#else
+    /* append temporary file name part */
+    strncat(path, ".XXXXXX", PATH_MAX - strlen(path) - 1);
+    if (mktemp(path)) {
+        strncat(path, ".sock", PATH_MAX - strlen(path) - 1);
+        fd = open(path, O_RDWR | O_CREAT | O_EXCL, 0600);
+        if (-1 == fd) {
+            close(fd);
+            unlink(path);
+        }
+    }
+#endif
 
     *socket_path = strdup(path);
     CHECK_NULL_NOMEM_RETURN(*socket_path);
