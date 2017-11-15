sysrepo/sysrepo

commit d119b61a46930d9eda5c21d103c984db1418cd85

FEATURE sysrepoctl support for multiple search directories
Fixes #919

src/executables/sysrepoctl.c
@@ -891,7 +891,7 @@ cleanup:
  * @brief Performs the --install operation.
  */
 static int
-srctl_install(const char *yang, const char *yin, const char *owner, const char *permissions, const char *search_dir)
+srctl_install(const char *yang, const char *yin, const char *owner, const char *permissions, char **search_dirs, int search_dir_count)
 {
     sr_conn_ctx_t *connection = NULL;
     sr_session_ctx_t *session = NULL;
@@ -909,20 +909,25 @@ srctl_install(const char *yang, const char *yin, const char *owner, const char *
     printf("Installing a new module from file '%s'...\n", (NULL != yang) ? yang : yin);
 
     /* extract the search directory path */
-    if (NULL == search_dir) {
-        search_dir = srctl_get_dir_path((NULL != yang) ? yang : yin);
-        if (NULL == search_dir) {
+    if (NULL == search_dirs) {
+        search_dirs = calloc(1, sizeof *search_dirs);
+        search_dirs[0] = srctl_get_dir_path((NULL != yang) ? yang : yin);
+        if (NULL == search_dirs[0]) {
             fprintf(stderr, "Error: Unable to extract search directory path.\n");
             goto fail;
         }
+        search_dir_count = 1;
         local_search_dir = true;
     }
 
     /* init libyang context */
-    ly_ctx = ly_ctx_new(search_dir, 0);
+    ly_ctx = ly_ctx_new(search_dirs[0], 0);
     if (NULL == ly_ctx) {
         fprintf(stderr, "Error: Unable to initialize libyang context: %s.\n", ly_errmsg());
         goto fail;
+    }
+    for (int i = 1; i < search_dir_count; ++i) {
+        ly_ctx_set_searchdir(ly_ctx, search_dirs[i]);
     }
 
     /* init module dependencies context */
@@ -1042,7 +1047,7 @@ cleanup:
     md_destroy(md_ctx);
     ly_ctx_destroy(ly_ctx, NULL);
     if (local_search_dir) {
-        free((char*)search_dir);
+        free((char*)search_dirs);
     }
     return rc;
 }
@@ -1118,6 +1123,7 @@ srctl_print_help()
     printf("  -o, --owner            Owner user and group of the module's data in chown format (--install, --change operations).\n");
     printf("  -p, --permissions      Access permissions of the module's data in chmod format (--install, --change operations).\n");
     printf("  -s, --search-dir       Directory to search for included/imported modules. Defaults to the directory with the YANG file being installed. (--install operation).\n");
+    printf("                         Can be specified multiple times.\n");
     printf("  -S, --search-installed Search for included/imported modules in sysrepo schema directory. (--install operation).\n");
     printf("\n");
     printf("Examples:\n");
@@ -1139,7 +1145,8 @@ main(int argc, char* argv[])
     char *feature_name = NULL;
     char *yang = NULL, *yin = NULL, *module = NULL, *revision = NULL;
     char *owner = NULL, *permissions = NULL;
-    char *search_dir = NULL;
+    char **search_dirs = NULL;
+    int search_dir_count = 0;
     char local_schema_search_dir[PATH_MAX] = { 0, }, local_data_search_dir[PATH_MAX] = { 0, };
     char local_internal_schema_search_dir[PATH_MAX] = { 0, }, local_internal_data_search_dir[PATH_MAX] = { 0, };
     int rc = SR_ERR_OK;
@@ -1211,7 +1218,9 @@ main(int argc, char* argv[])
                 permissions = optarg;
                 break;
             case 's':
-                search_dir = optarg;
+                ++search_dir_count;
+                search_dirs = realloc(search_dirs, search_dir_count * sizeof *search_dirs);
+                search_dirs[search_dir_count - 1] = optarg;
                 break;
             case 'S':
                 search_installed = 1;
@@ -1246,7 +1255,9 @@ main(int argc, char* argv[])
     }
 
     if (search_installed) {
-        search_dir = srctl_schema_search_dir;
+        search_dirs = calloc(1, sizeof *search_dirs);
+        search_dirs[0] = srctl_schema_search_dir;
+        search_dir_count = 1;
     }
 
     /* set log levels */
@@ -1263,7 +1274,7 @@ main(int argc, char* argv[])
             rc = srctl_list_modules();
             break;
         case 'i':
-            rc = srctl_install(yang, yin, owner, permissions, search_dir);
+            rc = srctl_install(yang, yin, owner, permissions, search_dirs, search_dir_count);
             break;
         case 'u':
             rc = srctl_uninstall(module, revision);
@@ -1281,5 +1292,6 @@ main(int argc, char* argv[])
             srctl_print_help();
     }
 
+    free(search_dirs);
     return (SR_ERR_OK == rc) ? EXIT_SUCCESS : EXIT_FAILURE;
 }

commit e47f8efdbedf7a90e688f9d732c9ee8882a8c669

sysrepoctl: Fix memory leak
Commit d119b61a introduced a memory leak by forgetting to free the
individual members of that array.

Discovered by ASAN; a simply `make install` was failing.

src/executables/sysrepoctl.c
@@ -1047,7 +1047,10 @@ cleanup:
     md_destroy(md_ctx);
     ly_ctx_destroy(ly_ctx, NULL);
     if (local_search_dir) {
-        free((char*)search_dirs);
+        for (int i = 0; i < search_dir_count; ++i) {
+            free(search_dirs[i]);
+        }
+        free(search_dirs);
     }
     return rc;
 }