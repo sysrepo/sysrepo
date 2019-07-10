f540129963ec8ae7f83719e6a122585f99cdeb22
Michal Vasko <mvasko@cesnet.cz>
2019-07-10 14:41:26 +0200

    sysrepo CHANGE licence changed to Apache 2.0

--- src/shm_sub.c
+++ src/shm_sub.c
@@ -4,13 +4,20 @@
  * @brief subscription SHM routines
  *
  * @copyright
- * Copyright (c) 2019 CESNET, z.s.p.o.
+ * Copyright 2018 Deutsche Telekom AG.
+ * Copyright 2018 - 2019 CESNET, z.s.p.o.
  *
- * This source code is licensed under BSD 3-Clause License (the "License").
- * You may not use this file except in compliance with the License.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     https://opensource.org/licenses/BSD-3-Clause
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 #include "common.h"
 
