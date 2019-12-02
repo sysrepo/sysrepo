53c30cb7538f2db1783af3a65665ad65dcc2e200
Michal Vasko <mvasko@cesnet.cz>
2019-12-02 13:44:58 +0100

    mod shm MAINTENANCE missing include headers

--- src/shm_mod.c
+++ src/shm_mod.c
@@ -26,6 +26,9 @@
 #include <time.h>
 #include <assert.h>
 #include <sys/types.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 #include <unistd.h>
 
 #include <libyang/libyang.h>
