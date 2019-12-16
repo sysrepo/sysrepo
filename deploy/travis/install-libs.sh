e4bd985dfa4d13bbafea819b8ce4beb7a79818e8
Michal Vasko <mvasko@cesnet.cz>
2019-12-16 11:28:41 +0100

    deploy NEW travis and codecov files

---
+++ deploy/travis/install-libs.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+set -e
+
+INSTALL_PREFIX_DIR=$HOME/local
+export PKG_CONFIG_PATH=$INSTALL_PREFIX_DIR/lib/pkgconfig:$PKG_CONFIG_PATH
+
+sudo apt-get install --reinstall ca-certificates
+sudo apt-get install software-properties-common # add-apt-repository tool
+sudo apt-get update -qq
+sudo apt-get install -y --force-yes valgrind coreutils python-dev gdb acl swig libcmocka-dev
+pip install --user codecov
+echo -n | openssl s_client -connect scan.coverity.com:443 | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' | sudo tee -a /etc/ssl/certs/ca-certificates.crt
+
+# libyang
+if [[ (( "$TRAVIS_BRANCH" == *"master"* )) || (( "$TRAVIS_TAG" =~ "v"[0-9]+"."[0-9]+"."[0-9]+ )) ]]; then
+    git clone https://github.com/CESNET/libyang.git
+else
+    git clone -b devel https://github.com/CESNET/libyang.git
+fi
+cd libyang ; mkdir build ; cd build
+cmake -DCMAKE_BUILD_TYPE=Debug -DENABLE_BUILD_TESTS=OFF ..
+make -j2 > /dev/null && sudo make install
+cd ../..
