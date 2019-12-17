466d998387336267bea0d0910abd596f1a475455
Michal Vasko <mvasko@cesnet.cz>
2019-12-17 14:49:05 +0100

    travis BUGFIX install missing libpcre3-dev package

--- deploy/travis/install-libs.sh
+++ deploy/travis/install-libs.sh
@@ -7,7 +7,7 @@ export PKG_CONFIG_PATH=$INSTALL_PREFIX_DIR/lib/pkgconfig:$PKG_CONFIG_PATH
 sudo apt-get install --reinstall ca-certificates
 sudo apt-get install software-properties-common # add-apt-repository tool
 sudo apt-get update -qq
-sudo apt-get install -y --force-yes valgrind coreutils python3-dev gdb acl swig libcmocka-dev
+sudo apt-get install -y --force-yes valgrind coreutils python3-dev libpcre3-dev gdb acl swig libcmocka-dev
 pip install --user codecov
 echo -n | openssl s_client -connect scan.coverity.com:443 | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' | sudo tee -a /etc/ssl/certs/ca-certificates.crt
 
