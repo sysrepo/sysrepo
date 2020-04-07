38ea442a061f7e40c50dece3538922141e66b1fb
Michal Vasko <mvasko@cesnet.cz>
2020-04-07 14:48:08 +0200

    over MAINTENANCE add missing header

--- examples/plugin/oven.c
+++ examples/plugin/oven.c
@@ -1,3 +1,18 @@
+/**
+ * @file oven.c
+ * @author Michal Vasko <mvasko@cesnet.cz>
+ * @brief oven example plugin
+ *
+ * @copyright
+ * Copyright (c) 2019 CESNET, z.s.p.o.
+ *
+ * This source code is licensed under BSD 3-Clause License (the "License").
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://opensource.org/licenses/BSD-3-Clause
+ */
+
 #include <stdlib.h>
 #include <string.h>
 #include <pthread.h>
