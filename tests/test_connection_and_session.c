d8657e6f1b24f92efb5d270f15f04d12588c7ee6
fredgan <ganshaolong@vip.qq.com>
2020-04-24 11:06:12 +0800

    add other api testcases for connection and session

--- tests/test_connection_and_session.c
+++ tests/test_connection_and_session.c
@@ -60,7 +60,12 @@ setup(void **state)
     if (sr_install_module(st->conn, TESTS_DIR "/files/test.yang", TESTS_DIR "/files", NULL, 0) != SR_ERR_OK) {
         return 1;
     }
-
+    if (sr_install_module(st->conn, TESTS_DIR "/files/ops-ref.yang", TESTS_DIR "/files", NULL, 0) != SR_ERR_OK) {
+        return 1;
+    }
+    if (sr_install_module(st->conn, TESTS_DIR "/files/ops.yang", TESTS_DIR "/files", NULL, 0) != SR_ERR_OK) {
+        return 1;
+    }
     sr_disconnect(st->conn);
 
     if (sr_connect(0, &(st->conn)) != SR_ERR_OK) {
@@ -80,6 +85,8 @@ teardown(void **state)
     struct state *st = (struct state *)*state;
 
     sr_remove_module(st->conn, "test");
+    sr_remove_module(st->conn, "ops");
+    sr_remove_module(st->conn, "ops-ref");
 
     sr_disconnect(st->conn);
     free(st);
@@ -156,7 +163,7 @@ test_set_diff_check_callback(void **state)
             }
             break;
         }
-        if (strncmp(entry->pw_name, pwd->pw_name, strlen(pwd->pw_name))) { 
+        if (strncmp(entry->pw_name, pwd->pw_name, strlen(pwd->pw_name))) {
             user = entry->pw_name;
             break;
         }
@@ -198,12 +205,232 @@ test_set_diff_check_callback(void **state)
     }
 }
 
+static void
+test_connect(void **state)
+{
+    struct state *st = (struct state *)*state;
+    int ret;
+
+    /* param error, connection NULL */
+    ret = sr_connect(0, NULL);
+    assert_int_equal(ret, SR_ERR_INVAL_ARG);
+
+    st = calloc(1, sizeof *st);
+    ret = sr_connect(0, &(st->conn));
+    assert_int_equal(ret, SR_ERR_OK);
+
+    sr_disconnect(st->conn);
+    free(st);
+}
+
+static int
+rpc_sub_cb(sr_session_ctx_t *session, const char *op_path, const sr_val_t *input, const size_t input_cnt,
+sr_event_t event, uint32_t request_id, sr_val_t **output, size_t *output_cnt, void *private_data)
+{
+    (void)session;
+    (void)op_path;
+    (void)input;
+    (void)input_cnt;
+    (void)event;
+    (void)request_id;
+    (void)output;
+    (void)output_cnt;
+    (void)private_data;
+
+    return SR_ERR_OK;
+}
+
+static void
+test_disconnect(void **state)
+{
+    struct state *st = (struct state *)*state;
+    sr_session_ctx_t *sess1 = NULL;
+    sr_session_ctx_t *sess2 = NULL;
+    sr_subscription_ctx_t *subscr = NULL;
+    int ret;
+
+    /* connection NULL */
+    sr_conn_ctx_t *conn = NULL;
+    ret = sr_disconnect(conn);
+    assert_int_equal(ret, SR_ERR_OK);
+
+    /* start session #1 */
+    ret = sr_session_start(st->conn, SR_DS_RUNNING, &sess1);
+    assert_int_equal(ret, SR_ERR_OK);
+
+    /* start session #2 */
+    ret = sr_session_start(st->conn, SR_DS_RUNNING, &sess2);
+    assert_int_equal(ret, SR_ERR_OK);
+
+    /* session #1 subscribe rpc1 */
+    ret = sr_rpc_subscribe(sess1, "/ops:rpc1", rpc_sub_cb, NULL, 0, 0, &subscr);
+    assert_int_equal(ret, SR_ERR_OK);
+
+    /* session #2 subscribe rpc2 */
+    ret = sr_rpc_subscribe(sess2, "/ops:rpc2", rpc_sub_cb, NULL, 0, 0, &subscr);
+    assert_int_equal(ret, SR_ERR_OK);
+
+    /* unsubscribes subscr */
+    ret = sr_unsubscribe(subscr);
+    assert_int_equal(ret, SR_ERR_OK);
+}
+
+static void
+test_get_context(void **state)
+{
+    struct state *st = (struct state *)*state;
+    const struct ly_ctx *ly_ctx;
+
+    /* connection NULL */
+    ly_ctx = sr_get_context(NULL);
+    assert_null(ly_ctx);
+
+    ly_ctx = sr_get_context(st->conn);
+    assert_non_null(ly_ctx);
+}
+
+static void
+test_session_notif_buffer(void **state)
+{
+    struct state *st = (struct state *)*state;
+    int ret;
+
+    ret = sr_session_notif_buffer(st->sess);
+    assert_int_equal(ret, SR_ERR_OK);
+}
+
+static void
+test_session_switch_and_get_ds(void **state)
+{
+    struct state *st = (struct state *)*state;
+    sr_datastore_t ds;
+    int ret;
+
+    /* param error,session NULL */
+    sr_session_ctx_t *sess = NULL;
+    ret = sr_session_switch_ds(sess, SR_DS_RUNNING);
+    assert_int_equal(ret, SR_ERR_INVAL_ARG);
+
+    /* get datastore, session NULL */
+    ds = sr_session_get_ds(sess);
+    assert_int_equal(ds, 0);
+
+    /* switch to candidate datastore */
+    ret = sr_session_switch_ds(st->sess, SR_DS_CANDIDATE);
+    assert_int_equal(ret, SR_ERR_OK);
+
+    /* get datastore */
+    ds = sr_session_get_ds(st->sess);
+    assert_int_equal(ds, SR_DS_CANDIDATE);
+}
+
+static void
+test_session_get_id(void **state)
+{
+    struct state *st = (struct state *)*state;
+    uint32_t sid;
+
+    /* session NULL*/
+    sr_session_ctx_t*sess=NULL;
+    sid = sr_session_get_id(sess);
+    assert_int_equal(sid, 0);
+
+    sid = sr_session_get_id(st->sess);
+}
+
+static void
+test_session_set_and_get_nc_id(void **state)
+{
+    struct state *st = (struct state *)*state;
+    uint32_t nc_sid;
+
+    /* session NULL */
+    sr_session_ctx_t *sess = NULL;
+    sr_session_set_nc_id(sess, 64);
+    nc_sid = sr_session_get_nc_id(sess);
+    assert_int_equal(nc_sid, 0);
+
+    sr_session_set_nc_id(st->sess, 64);
+    nc_sid = sr_session_get_nc_id(st->sess);
+    assert_int_equal(nc_sid, 64);
+}
+
+static void
+test_session_set_and_get_user(void **state)
+{
+    struct state *st =  (struct state *)*state;
+    struct passwd *pwd;
+    const char *user;
+    int ret;
+
+    pwd = getpwuid(getuid());
+
+    /* params error,session NULL or user NULL */
+    sr_session_ctx_t *sess = NULL;
+    ret = sr_session_set_user(sess, pwd->pw_name);
+    assert_int_equal(ret, SR_ERR_INVAL_ARG);
+    ret = sr_session_set_user(st->sess, NULL);
+    assert_int_equal(ret, SR_ERR_INVAL_ARG);
+
+    user = sr_session_get_user(sess);
+    assert_null(user);
+
+    /* invalid user */
+    ret = sr_session_set_user(st->sess, "no user");
+    /* not a root*/
+    if (geteuid()) {
+        assert_int_equal(ret, SR_ERR_UNAUTHORIZED);
+    } else {
+        assert_int_equal(ret, SR_ERR_NOT_FOUND);
+    }
+
+    ret = sr_session_set_user(st->sess, pwd->pw_name);
+    /* not a root*/
+    if (geteuid()) {
+        assert_int_equal(ret, SR_ERR_UNAUTHORIZED);
+    } else {
+        assert_int_equal(ret, SR_ERR_OK);
+    }
+
+    user = sr_session_get_user(st->sess);
+    /* not a root*/
+   if (geteuid()) {
+       assert_null(user);
+    } else {
+        assert_string_equal(user, pwd->pw_name);
+    }
+}
+
+static void
+test_session_get_connection(void **state)
+{
+    struct state *st = (struct state *)*state;
+    sr_conn_ctx_t *conn = NULL;
+
+    /* sesssion NULL */
+    sr_session_ctx_t *sess = NULL;
+    conn = sr_session_get_connection(sess);
+    assert_null(conn);
+
+    conn = sr_session_get_connection(st->sess);
+    assert_non_null(conn);
+}
+
 /* MAIN */
 int
 main(void)
 {
     const struct CMUnitTest tests[] = {
         cmocka_unit_test_setup_teardown(test_set_diff_check_callback, setup, teardown),
+        cmocka_unit_test(test_connect),
+        cmocka_unit_test_setup_teardown(test_disconnect, setup, teardown),
+        cmocka_unit_test_setup_teardown(test_get_context, setup, teardown),
+        cmocka_unit_test_setup_teardown(test_session_notif_buffer, setup, teardown),
+        cmocka_unit_test_setup_teardown(test_session_switch_and_get_ds, setup, teardown),
+        cmocka_unit_test_setup_teardown(test_session_get_id, setup, teardown),
+        cmocka_unit_test_setup_teardown(test_session_set_and_get_nc_id, setup, teardown),
+        cmocka_unit_test_setup_teardown(test_session_set_and_get_user, setup, teardown),
+        cmocka_unit_test_setup_teardown(test_session_get_connection, setup, teardown),
     };
 
     setenv("CMOCKA_TEST_ABORT", "1", 1);
