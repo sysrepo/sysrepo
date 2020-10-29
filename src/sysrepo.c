sysrepo/sysrepo

commit f36f312e100f363d0d9a56a5ff415513523e3fcb

sysrepo CHANGE add special enabled event
... which is triggered even on empty data.

src/sysrepo.c
@@ -2842,34 +2842,31 @@ sr_module_change_subscribe_running_enable(sr_session_ctx_t *session, const struc
         }
     }
 
-    if (mod_info.diff) {
-        tmp_sess.conn = conn;
-        tmp_sess.ds = mod_info.ds;
-        tmp_sess.dt[tmp_sess.ds].diff = mod_info.diff;
+    tmp_sess.conn = conn;
+    tmp_sess.ds = mod_info.ds;
+    tmp_sess.dt[tmp_sess.ds].diff = mod_info.diff;
 
-        if (!(opts & SR_SUBSCR_DONE_ONLY)) {
-            tmp_sess.ev = SR_SUB_EV_CHANGE;
-            SR_LOG_INF("Triggering \"%s\" \"%s\" event on enabled data.", ly_mod->name, sr_ev2str(tmp_sess.ev));
-
-            /* present all changes in a regular "change" event */
-            err_code = callback(&tmp_sess, ly_mod->name, xpath, sr_ev2api(tmp_sess.ev), private_data);
-            if (err_code != SR_ERR_OK) {
-                /* callback failed but it is the only one so no "abort" event is necessary */
-                sr_errinfo_new(&err_info, SR_ERR_CALLBACK_FAILED, NULL, "Subscribing to \"%s\" changes failed.", ly_mod->name);
-                if (tmp_sess.err_info && (tmp_sess.err_info->err_code == SR_ERR_OK)) {
-                    /* remember callback error info */
-                    sr_errinfo_merge(&err_info, tmp_sess.err_info);
-                }
-                goto cleanup_mods_unlock;
-            }
-        }
-
-        /* finish with a "done" event just because this event should imitate a regular configuration change */
-        tmp_sess.ev = SR_SUB_EV_DONE;
+    if (!(opts & SR_SUBSCR_DONE_ONLY)) {
+        tmp_sess.ev = SR_SUB_EV_ENABLED;
         SR_LOG_INF("Triggering \"%s\" \"%s\" event on enabled data.", ly_mod->name, sr_ev2str(tmp_sess.ev));
 
-        callback(&tmp_sess, ly_mod->name, xpath, sr_ev2api(tmp_sess.ev), private_data);
+        /* present all changes in an "enabled" event */
+        err_code = callback(&tmp_sess, ly_mod->name, xpath, sr_ev2api(tmp_sess.ev), private_data);
+        if (err_code != SR_ERR_OK) {
+            /* callback failed but it is the only one so no "abort" event is necessary */
+            sr_errinfo_new(&err_info, SR_ERR_CALLBACK_FAILED, NULL, "Subscribing to \"%s\" changes failed.", ly_mod->name);
+            if (tmp_sess.err_info && (tmp_sess.err_info->err_code == SR_ERR_OK)) {
+                /* remember callback error info */
+                sr_errinfo_merge(&err_info, tmp_sess.err_info);
+            }
+            goto cleanup_mods_unlock;
+        }
     }
+
+    /* finish with a "done" event just because this event should imitate a regular configuration change */
+    tmp_sess.ev = SR_SUB_EV_DONE;
+    SR_LOG_INF("Triggering \"%s\" \"%s\" event on enabled data.", ly_mod->name, sr_ev2str(tmp_sess.ev));
+    callback(&tmp_sess, ly_mod->name, xpath, sr_ev2api(tmp_sess.ev), private_data);
 
     /* success */
 
@@ -3083,7 +3080,7 @@ sr_get_changes_iter(sr_session_ctx_t *session, const char *xpath, sr_change_iter
     SR_CHECK_ARG_APIRET(!session || !IS_WRITABLE_DS(session->ds) || (session->ev == SR_SUB_EV_NONE) || !xpath || !iter,
             session, err_info);
 
-    if (!session->dt[session->ds].diff) {
+    if ((session->ev != SR_SUB_EV_ENABLED) && (session->ev != SR_SUB_EV_DONE) && !session->dt[session->ds].diff) {
         sr_errinfo_new(&err_info, SR_ERR_INVAL_ARG, NULL, "Session without changes.");
         return sr_api_ret(session, err_info);
     }
@@ -3094,7 +3091,11 @@ sr_get_changes_iter(sr_session_ctx_t *session, const char *xpath, sr_change_iter
         return sr_api_ret(session, err_info);
     }
 
-    (*iter)->set = lyd_find_path(session->dt[session->ds].diff, xpath);
+    if (session->dt[session->ds].diff) {
+        (*iter)->set = lyd_find_path(session->dt[session->ds].diff, xpath);
+    } else {
+        (*iter)->set = ly_set_new();
+    }
     SR_CHECK_MEM_GOTO(!(*iter)->set, err_info, error);
     (*iter)->idx = 0;
 

tests/test_operational.c
@@ -110,9 +110,11 @@ enabled_change_cb(sr_session_ctx_t *session, const char *module_name, const char
         assert_string_equal(module_name, "ietf-interfaces");
 
         if (*called == 0) {
-            assert_int_equal(event, SR_EV_CHANGE);
-        } else {
+            assert_int_equal(event, SR_EV_ENABLED);
+        } else if (*called == 1) {
             assert_int_equal(event, SR_EV_DONE);
+        } else {
+            fail();
         }
 
         /* get changes iter */
@@ -174,6 +176,26 @@ enabled_change_cb(sr_session_ctx_t *session, const char *module_name, const char
         assert_int_equal(new_val->dflt, 1);
 
         sr_free_val(new_val);
+
+        /* no more changes */
+        ret = sr_get_change_next(session, iter, &op, &old_val, &new_val);
+        assert_int_equal(ret, SR_ERR_NOT_FOUND);
+
+        sr_free_change_iter(iter);
+    } else if (!strcmp(xpath, "/ietf-interfaces:interfaces/interface[name='eth256']")) {
+        assert_string_equal(module_name, "ietf-interfaces");
+
+        if (*called == 0) {
+            assert_int_equal(event, SR_EV_ENABLED);
+        } else if (*called == 1) {
+            assert_int_equal(event, SR_EV_DONE);
+        } else {
+            fail();
+        }
+
+        /* get changes iter */
+        ret = sr_get_changes_iter(session, "/ietf-interfaces:*//.", &iter);
+        assert_int_equal(ret, SR_ERR_OK);
 
         /* no more changes */
         ret = sr_get_change_next(session, iter, &op, &old_val, &new_val);
@@ -259,7 +281,7 @@ test_enabled_partial(void **state)
     ret = sr_module_change_subscribe(st->sess, "ietf-interfaces", "/ietf-interfaces:interfaces/interface[name='eth256']",
             enabled_change_cb, &called, 0, SR_SUBSCR_ENABLED, &subscr);
     assert_int_equal(ret, SR_ERR_OK);
-    assert_int_equal(called, 0);
+    assert_int_equal(called, 2);
 
     /* "operational" should be empty again */
     ret = sr_session_switch_ds(st->sess, SR_DS_OPERATIONAL);

src/common.c
@@ -2347,6 +2347,8 @@ sr_ev2str(sr_sub_event_t ev)
         return "done";
     case SR_SUB_EV_ABORT:
         return "abort";
+    case SR_SUB_EV_ENABLED:
+        return "enabled";
     case SR_SUB_EV_OPER:
         return "operational";
     case SR_SUB_EV_RPC:
@@ -2376,6 +2378,8 @@ sr_ev2api(sr_sub_event_t ev)
         return SR_EV_DONE;
     case SR_SUB_EV_ABORT:
         return SR_EV_ABORT;
+    case SR_SUB_EV_ENABLED:
+        return SR_EV_ENABLED;
     case SR_SUB_EV_RPC:
         return SR_EV_RPC;
     default:

src/shm.h
@@ -193,6 +193,7 @@ typedef enum sr_sub_event_e {
     SR_SUB_EV_CHANGE,           /**< New change event ready. */
     SR_SUB_EV_DONE,             /**< New done event ready. */
     SR_SUB_EV_ABORT,            /**< New abort event ready. */
+    SR_SUB_EV_ENABLED,          /**< New enabled event ready. */
     SR_SUB_EV_OPER,             /**< New operational event ready. */
     SR_SUB_EV_RPC,              /**< New RPC/action event ready. */
     SR_SUB_EV_NOTIF,            /**< New notification event ready. */
@@ -200,7 +201,8 @@ typedef enum sr_sub_event_e {
 
 /** Whether an event is one to be processed by the listeners (subscribers). */
 #define SR_IS_LISTEN_EVENT(ev) ((ev == SR_SUB_EV_UPDATE) || (ev == SR_SUB_EV_CHANGE) || (ev == SR_SUB_EV_DONE) \
-        || (ev == SR_SUB_EV_ABORT) || (ev == SR_SUB_EV_OPER) || (ev == SR_SUB_EV_RPC) || (ev == SR_SUB_EV_NOTIF))
+        || (ev == SR_SUB_EV_ABORT) || (ev == SR_SUB_EV_ENABLED) || (ev == SR_SUB_EV_OPER) || (ev == SR_SUB_EV_RPC) \
+        || (ev == SR_SUB_EV_NOTIF))
 
 /** Whether an event is one to be processed by the originators. */
 #define SR_IS_NOTIFY_EVENT(ev) ((ev == SR_SUB_EV_SUCCESS) || (ev == SR_SUB_EV_ERROR))

src/sysrepo.h
@@ -1005,6 +1005,9 @@ typedef enum sr_subscr_flag_e {
 
     /**
      * @brief The subscriber wants to be notified about the current configuration at the moment of subscribing.
+     * It will receive ::SR_EV_ENABLED event, whose applying can fail causing the whole subscription to fail.
+     * On success this event will be followed by ::SR_EV_DONE. Be careful, ::SR_EV_ENABLED will be triggered
+     * even if there are no data so there will not be any changes!
      */
     SR_SUBSCR_ENABLED = 16,
 
@@ -1102,6 +1105,9 @@ typedef enum sr_event_e {
                         This event is also generated for RPC subscriptions when a later callback has failed and
                         this one has already successfully processed ::SR_EV_RPC. The callback that failed will never
                         get this event! */
+    SR_EV_ENABLED, /**< Occurs for subscriptions with the flag ::SR_SUBSCR_ENABLED and is normally followed by
+                        ::SR_EV_DONE. It can fail and will also be triggered even when there is no startup configuration
+                        (which is different from the ::SR_EV_CHANGE event). */
     SR_EV_RPC,     /**< Occurs for a standard RPC execution. If a later callback fails, ::SR_EV_ABORT is generated. */
 } sr_event_t;
 

commit 4eba86ca7aa5224296f410a41188e3a62a2eaffa

Do not let SR_SUBSCR_DONE_ONLY mask out SR_EV_ENABLED events
The SR_SUBSCR_DONE_ONLY event is documented to control whether
SR_EV_CHANGE/SR_EV_ABORT are sent. Since the rewrite, it used to also
skip generating the initial SR_EV_ENABLED as well. That's surely a
mistake because that one is supposed to be gated by the SR_EV_ENABLED
flag.

Fixes: f36f312e ("sysrepo CHANGE add special enabled event")

src/sysrepo.c
@@ -3260,7 +3260,7 @@ sr_module_change_subscribe_running_enable(sr_session_ctx_t *session, const struc
     tmp_sess.ds = SR_DS_RUNNING;
     tmp_sess.dt[tmp_sess.ds].diff = enabled_data;
 
-    if (!(opts & SR_SUBSCR_DONE_ONLY)) {
+    if (opts & SR_SUBSCR_ENABLED) {
         tmp_sess.ev = SR_SUB_EV_ENABLED;
         SR_LOG_INF("Triggering \"%s\" \"%s\" event on enabled data.", ly_mod->name, sr_ev2str(tmp_sess.ev));
 

tests/test_operational.c
@@ -814,7 +814,7 @@ test_enabled_partial(void **state)
 
     called = 0;
     ret = sr_module_change_subscribe(st->sess, "ietf-interfaces", "/ietf-interfaces:interfaces/interface[name='eth128']",
-            enabled_change_cb, &called, 0, SR_SUBSCR_ENABLED, &subscr);
+            enabled_change_cb, &called, 0, SR_SUBSCR_ENABLED | SR_SUBSCR_DONE_ONLY, &subscr);
     assert_int_equal(ret, SR_ERR_OK);
     assert_int_equal(called, 2);
 