6542b8eeee228558e01b04e7a4b908e9149177e6
Michal Vasko <mvasko@cesnet.cz>
2021-02-02 18:46:18 +0100

    shm BUGFIX increase SHM version

--- src/shm.h
+++ src/shm.h
@@ -32,7 +32,7 @@
 #include "common.h"
 
 #define SR_MAIN_SHM_LOCK "sr_main_lock"     /**< Main SHM file lock name. */
-#define SR_SHM_VER 5                        /**< Main and ext SHM version of their expected content structures. */
+#define SR_SHM_VER 6                        /**< Main and ext SHM version of their expected content structures. */
 
 /**
  * Main SHM organization
