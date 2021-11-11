sysrepo/sysrepo

commit 39bc5adb98026af355febcca5abf27d44ebf6f10

plugins FEATURE error message warning kernel param value

src/plugins/common_lyb.c
@@ -184,7 +184,7 @@ srplyb_log_err_ly(const char *plg_name, const struct ly_ctx *ly_ctx)
 }
 
 int
-srlyb_open(const char *pathname, int flags, mode_t mode)
+srlyb_open(const char *path, int flags, mode_t mode)
 {
     mode_t um;
     int fd;
@@ -200,7 +200,7 @@ srlyb_open(const char *pathname, int flags, mode_t mode)
     um = umask(SR_UMASK);
 
     /* open the file */
-    fd = open(pathname, flags, mode);
+    fd = open(path, flags, mode);
 
     /* restore umask (should not modify errno) */
     umask(um);
@@ -212,6 +212,29 @@ srlyb_open(const char *pathname, int flags, mode_t mode)
 
     /* success */
     return fd;
+}
+
+int
+srlyb_open_error(const char *plg_name, const char *path)
+{
+    FILE *f;
+    char buf[8] = "";
+
+    SRPLG_LOG_ERR(plg_name, "Opening \"%s\" failed (%s).", path, strerror(errno));
+    if ((errno == EACCES) && !geteuid()) {
+        /* check kernel parameter value of fs.protected_regular */
+        f = fopen("/proc/sys/fs/protected_regular", "r");
+        if (f) {
+            fgets(buf, 8, f);
+            fclose(f);
+        }
+    }
+    if (buf[0] && (atoi(buf) != 0)) {
+        SRPLG_LOG_ERR(plg_name, "Caused by kernel parameter \"fs.protected_regular\", which must be \"0\" "
+                "(currently \"%d\").", atoi(buf));
+    }
+
+    return SR_ERR_SYS;
 }
 
 int
@@ -436,16 +459,14 @@ srlyb_cp_path(const char *plg_name, const char *to, const char *from, mode_t fil
     /* open "from" file */
     fd_from = srlyb_open(from, O_RDONLY, 0);
     if (fd_from < 0) {
-        SRPLG_LOG_ERR(plg_name, "Opening \"%s\" failed (%s).", from, strerror(errno));
-        rc = SR_ERR_SYS;
+        rc = srlyb_open_error(plg_name, from);
         goto cleanup;
     }
 
     /* open "to" */
     fd_to = srlyb_open(to, O_WRONLY | O_TRUNC | O_CREAT, file_mode);
     if (fd_to < 0) {
-        SRPLG_LOG_ERR(plg_name, "Opening \"%s\" failed (%s).", to, strerror(errno));
-        rc = SR_ERR_SYS;
+        rc = srlyb_open_error(plg_name, to);
         goto cleanup;
     }
 

src/plugins/common_lyb.h
@@ -86,13 +86,22 @@ void srplyb_log_err_ly(const char *plg_name, const struct ly_ctx *ly_ctx);
  *
  * Additionally sets umask.
  *
- * @param[in] pathname Path of the file to open.
+ * @param[in] path Path of the file to open.
  * @param[in] flags Flags to use.
  * @param[in] mode Permissions for the file in case it is created.
  * @return Opened file descriptor.
  * @return -1 on error, errno set.
  */
-int srlyb_open(const char *pathname, int flags, mode_t mode);
+int srlyb_open(const char *path, int flags, mode_t mode);
+
+/**
+ * @brief Generate plugin error on failed open.
+ *
+ * @param[in] plg_name Plugin name.
+ * @param[in] path Path of the file to open.
+ * @return SR err value to return.
+ */
+int srlyb_open_error(const char *plg_name, const char *path);
 
 /**
  * @brief Get the UID of a user or vice versa.

src/plugins/ds_lyb.c
@@ -72,8 +72,7 @@ srpds_lyb_store_(const struct lys_module *mod, sr_datastore_t ds, const struct l
 
     /* open the file */
     if ((fd = srlyb_open(path, O_WRONLY | O_CREAT, mode)) == -1) {
-        SRPLG_LOG_ERR(srpds_name, "Opening \"%s\" failed (%s).", path, strerror(errno));
-        rc = SR_ERR_SYS;
+        rc = srlyb_open_error(srpds_name, path);
         goto cleanup;
     }
 
@@ -306,8 +305,7 @@ retry_open:
             }
         }
 
-        SRPLG_LOG_ERR(srpds_name, "Opening \"%s\" failed (%s).", path, strerror(errno));
-        rc = SR_ERR_SYS;
+        rc = srlyb_open_error(srpds_name, path);
         goto cleanup;
     }
 

src/plugins/ntf_lyb.c
@@ -191,8 +191,7 @@ srpntf_open_file(const char *mod_name, time_t from_ts, time_t to_ts, int flags, 
 
     *notif_fd = srlyb_open(path, flags, perm);
     if (*notif_fd == -1) {
-        SRPLG_LOG_ERR(srpntf_name, "Opening \"%s\" failed (%s).", path, strerror(errno));
-        rc = SR_ERR_SYS;
+        rc = srlyb_open_error(srpntf_name, path);
         goto cleanup;
     }
 

commit 265a31d47dac89671384f26d92623f827ed5d1c5

Fix -Wunused-result
My compiler warns about not checking the fgets()'s result. Since this is
really an opportunistic check, nothing else, and since this thing can
fail on some system configurations, simply silence that warning. The
worst thing which can happen is a less useful warning.

Also, don't repeat a magic number (buffer size), use a sizeof for this.

Fixes: 39bc5adb9 (plugins FEATURE error message warning kernel param value)

src/plugins/common_lyb.c
@@ -225,7 +225,7 @@ srlyb_open_error(const char *plg_name, const char *path)
         /* check kernel parameter value of fs.protected_regular */
         f = fopen("/proc/sys/fs/protected_regular", "r");
         if (f) {
-            fgets(buf, 8, f);
+            (void)fgets(buf, sizeof(buf), f);
             fclose(f);
         }
     }