8acadd7ae18afcf14bf4fcbbd2a97bda98e52b11
Brett Bergquist <bbergquist@canoga.com>
2021-12-17 09:19:41 -0500

    updated oper_data_pull_example for testing operational with config

--- examples/oper_data_pull_example.c
+++ examples/oper_data_pull_example.c
@@ -35,15 +35,18 @@ dp_get_items_cb(sr_session_ctx_t *session, uint32_t sub_id, const char *module_n
 {
     (void)session;
     (void)sub_id;
-    (void)request_xpath;
     (void)request_id;
     (void)private_data;
 
-    printf("\n\n ========== DATA FOR \"%s\" \"%s\" REQUESTED =======================\n\n", module_name, xpath);
+    printf("\n\n ========== DATA FOR \"%s\" \"%s\" \"%s\" REQUESTED =======================\n\n", module_name, xpath, request_xpath);
 
-    if (!strcmp(module_name, "examples") && !strcmp(xpath, "/examples:stats")) {
-        lyd_new_path(NULL, sr_get_context(sr_session_get_connection(session)), "/examples:stats/counter", "852", 0, parent);
-        lyd_new_path(*parent, NULL, "/examples:stats/counter2", "1052", 0, NULL);
+    if (!strcmp(module_name, "examples")) {
+        if (strstr(request_xpath, "/examples:stats")) {
+            lyd_new_path(NULL, sr_get_context(sr_session_get_connection(session)), "/examples:stats/counter", "852", 0, parent);
+            lyd_new_path(*parent, NULL, "/examples:stats/counter2", "1052", 0, NULL);
+        } else if (strstr(request_xpath, "/examples:cont/l2")) {
+            lyd_new_path(NULL, sr_get_context(sr_session_get_connection(session)), "/examples:cont/l2", "plugh", 0, parent);
+        }
     }
 
     return SR_ERR_OK;
@@ -63,17 +66,20 @@ main(int argc, char **argv)
     sr_conn_ctx_t *connection = NULL;
     sr_session_ctx_t *session = NULL;
     sr_subscription_ctx_t *subscription = NULL;
+    sr_subscr_options_t opt = SR_SUBSCR_OPER_MERGE;
+//    sr_subscr_options_t opt = SR_SUBSCR_DEFAULT;
     int rc = SR_ERR_OK;
-    const char *mod_name, *path;
+    const char *mod_name;
 
-    if (argc != 3) {
-        printf("%s <module-to-provide-data-from> <path-to-provide>\n", argv[0]);
+    if (argc < 3) {
+        printf("%s <module-to-provide-data-from> <path-to-provide> [<path-to-provide> ...]\n", argv[0]);
         return EXIT_FAILURE;
     }
     mod_name = argv[1];
-    path = argv[2];
 
-    printf("Application will provide data \"%s\" of \"%s\".\n\n", path, mod_name);
+    for (int i = 2; i < argc; i++) {
+    	printf("Application will provide data \"%s\" of \"%s\".\n\n", argv[i], mod_name);
+    }
 
     /* turn logging on */
     sr_log_stderr(SR_LL_WRN);
@@ -90,10 +96,13 @@ main(int argc, char **argv)
         goto cleanup;
     }
 
-    /* subscribe for providing the operational data */
-    rc = sr_oper_get_items_subscribe(session, mod_name, path, dp_get_items_cb, NULL, 0, &subscription);
-    if (rc != SR_ERR_OK) {
-        goto cleanup;
+    for (int i = 2; i < argc; i++) {
+        /* subscribe for providing the operational data */
+        rc = sr_oper_get_items_subscribe(session, mod_name, argv[i], dp_get_items_cb, NULL, opt, &subscription);
+        if (rc != SR_ERR_OK) {
+            goto cleanup;
+        }
+        opt |= SR_SUBSCR_CTX_REUSE;
     }
 
     printf("\n\n ========== LISTENING FOR REQUESTS ==========\n\n");
