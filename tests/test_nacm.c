47781c86144abd4b6a251241148bd5c07cc5cb8b
Michal Vasko <mvasko@cesnet.cz>
2022-03-11 15:27:49 +0100

    nacm BUGFIX selected subtree filtering

--- tests/test_nacm.c
+++ tests/test_nacm.c
@@ -80,14 +80,27 @@ static int
 setup_basic_nacm(void **state)
 {
     struct state *st = (struct state *)*state;
+    const struct ly_ctx *ctx;
+    const char *data;
+    struct lyd_node *edit;
 
     /* set some data */
-    if (sr_set_item_str(st->sess, "/test:cont/l2[k='k1']/v", "10", NULL, 0)) {
+    data = "<cont xmlns=\"urn:test\">\n"
+            "  <l2>\n"
+            "    <k>k1</k>\n"
+            "    <v>10</v>\n"
+            "  </l2>\n"
+            "  <ll2>25</ll2>\n"
+            "</cont>\n";
+    ctx = sr_acquire_context(st->conn);
+    if (lyd_parse_data_mem(ctx, data, LYD_XML, LYD_PARSE_STRICT | LYD_PARSE_ONLY, 0, &edit)) {
         return 1;
     }
-    if (sr_set_item_str(st->sess, "/test:cont/ll2", "25", NULL, 0)) {
+    if (sr_edit_batch(st->sess, edit, "merge")) {
         return 1;
     }
+    lyd_free_siblings(edit);
+    sr_release_context(st->conn);
     if (sr_apply_changes(st->sess, 0)) {
         return 1;
     }
@@ -100,11 +113,33 @@ setup_basic_nacm(void **state)
     return 0;
 }
 
+static int
+dummy_rpc_cb(sr_session_ctx_t *session, uint32_t sub_id, const char *xpath, const sr_val_t *input,
+        const size_t input_cnt, sr_event_t event, uint32_t request_id, sr_val_t **output, size_t *output_cnt,
+        void *private_data)
+{
+    (void)session;
+    (void)sub_id;
+    (void)xpath;
+    (void)input;
+    (void)input_cnt;
+    (void)event;
+    (void)request_id;
+    (void)output;
+    (void)output_cnt;
+    (void)private_data;
+
+    return SR_ERR_OK;
+}
+
 static void
 test_basic(void **state)
 {
     struct state *st = (struct state *)*state;
+    sr_subscription_ctx_t *sub = NULL;
     sr_data_t *data;
+    sr_val_t *output;
+    size_t output_cnt;
     char *str;
     int ret;
 
@@ -131,13 +166,162 @@ test_basic(void **state)
     ret = sr_apply_changes(st->sess, 0);
     assert_int_equal(ret, SR_ERR_UNAUTHORIZED);
     sr_discard_changes(st->sess);
+
+    /* execute an operation, allowed by default */
+    ret = sr_rpc_subscribe(st->sess, "/test:r1", dummy_rpc_cb, NULL, 0, 0, &sub);
+    assert_int_equal(ret, SR_ERR_OK);
+    ret = sr_rpc_send(st->sess, "/test:r1", NULL, 0, 0, &output, &output_cnt);
+    assert_int_equal(ret, SR_ERR_OK);
+    sr_unsubscribe(sub);
+}
+
+static int
+setup_read_nacm(void **state)
+{
+    struct state *st = (struct state *)*state;
+    const struct ly_ctx *ctx;
+    const char *data;
+    struct lyd_node *edit;
+
+    /* set NACM and some data */
+    data = "<nacm xmlns=\"urn:ietf:params:xml:ns:yang:ietf-netconf-acm\">\n"
+            "  <read-default>deny</read-default>\n"
+            "  <enable-external-groups>false</enable-external-groups>\n"
+            "  <groups>\n"
+            "    <group>\n"
+            "      <name>test-group</name>\n"
+            "      <user-name>test-user</user-name>\n"
+            "    </group>\n"
+            "  </groups>\n"
+            "  <rule-list>\n"
+            "    <name>rule1</name>\n"
+            "    <group>test-group</group>\n"
+            "    <rule>\n"
+            "      <name>allow-key</name>\n"
+            "      <module-name>test</module-name>\n"
+            "      <path xmlns:t=\"urn:test\">/t:cont/t:l2/t:k</path>\n"
+            "      <access-operations>read</access-operations>\n"
+            "      <action>permit</action>\n"
+            "    </rule>\n"
+            "  </rule-list>\n"
+            "</nacm>\n"
+            "<cont xmlns=\"urn:test\">\n"
+            "  <l2>\n"
+            "    <k>k1</k>\n"
+            "    <v>10</v>\n"
+            "  </l2>\n"
+            "  <ll2>25</ll2>\n"
+            "</cont>\n";
+    ctx = sr_acquire_context(st->conn);
+    if (lyd_parse_data_mem(ctx, data, LYD_XML, LYD_PARSE_STRICT | LYD_PARSE_ONLY, 0, &edit)) {
+        return 1;
+    }
+    if (sr_edit_batch(st->sess, edit, "merge")) {
+        return 1;
+    }
+    lyd_free_siblings(edit);
+    sr_release_context(st->conn);
+    if (sr_apply_changes(st->sess, 0)) {
+        return 1;
+    }
+
+    /* set user */
+    if (sr_nacm_set_user(st->sess, "test-user")) {
+        return 1;
+    }
+
+    return 0;
+}
+
+static int
+teardown_nacm(void **state)
+{
+    struct state *st = (struct state *)*state;
+
+    /* clear user */
+    if (sr_nacm_set_user(st->sess, NULL)) {
+        return 1;
+    }
+
+    /* clear data */
+    if (sr_delete_item(st->sess, "/test:cont", 0)) {
+        return 1;
+    }
+    if (sr_delete_item(st->sess, "/ietf-netconf-acm:nacm", 0)) {
+        return 1;
+    }
+    if (sr_apply_changes(st->sess, 0)) {
+        return 1;
+    }
+
+    return 0;
+}
+
+static void
+test_read(void **state)
+{
+    struct state *st = (struct state *)*state;
+    sr_data_t *data;
+    char *str;
+    int ret;
+
+    /* read data #1 */
+    ret = sr_get_data(st->sess, "/test:*", 0, 0, 0, &data);
+    assert_int_equal(ret, SR_ERR_OK);
+
+    ret = lyd_print_mem(&str, data->tree, LYD_XML, LYD_PRINT_WITHSIBLINGS);
+    assert_int_equal(ret, LY_SUCCESS);
+    sr_release_data(data);
+    assert_string_equal(str,
+            "<cont xmlns=\"urn:test\">\n"
+            "  <l2>\n"
+            "    <k>k1</k>\n"
+            "  </l2>\n"
+            "</cont>\n");
+    free(str);
+
+    /* read data #2 */
+    ret = sr_get_data(st->sess, "/test:cont/l2", 0, 0, 0, &data);
+    assert_int_equal(ret, SR_ERR_OK);
+
+    ret = lyd_print_mem(&str, data->tree, LYD_XML, LYD_PRINT_WITHSIBLINGS);
+    assert_int_equal(ret, LY_SUCCESS);
+    sr_release_data(data);
+    assert_string_equal(str,
+            "<cont xmlns=\"urn:test\">\n"
+            "  <l2>\n"
+            "    <k>k1</k>\n"
+            "  </l2>\n"
+            "</cont>\n");
+    free(str);
+
+    /* read data #3 */
+    ret = sr_get_data(st->sess, "/test:cont/l2/k", 0, 0, 0, &data);
+    assert_int_equal(ret, SR_ERR_OK);
+
+    ret = lyd_print_mem(&str, data->tree, LYD_XML, LYD_PRINT_WITHSIBLINGS);
+    assert_int_equal(ret, LY_SUCCESS);
+    sr_release_data(data);
+    assert_string_equal(str,
+            "<cont xmlns=\"urn:test\">\n"
+            "  <l2>\n"
+            "    <k>k1</k>\n"
+            "  </l2>\n"
+            "</cont>\n");
+    free(str);
+
+    /* read no data */
+    ret = sr_get_data(st->sess, "/test:cont/l2/v", 0, 0, 0, &data);
+    assert_int_equal(ret, SR_ERR_OK);
+    assert_null(data);
 }
 
 int
 main(void)
 {
     const struct CMUnitTest tests[] = {
-        cmocka_unit_test_setup(test_basic, setup_basic_nacm),
+        cmocka_unit_test_setup_teardown(test_basic, setup_basic_nacm, teardown_nacm),
+        cmocka_unit_test_setup_teardown(test_read, setup_read_nacm, teardown_nacm),
     };
 
     setenv("CMOCKA_TEST_ABORT", "1", 1);
