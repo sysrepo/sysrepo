df89fc02e301cc8f2e6b30ec37b990f52ca1d5c4
Michal Vasko <mvasko@cesnet.cz>
2022-07-26 08:48:44 +0200

    error format BUGFIX missing period

--- src/utils/error_format.c
+++ src/utils/error_format.c
@@ -62,7 +62,7 @@ sr_session_set_netconf_error(sr_session_ctx_t *session, const char *error_type, 
     }
 
     /* generic error message */
-    sr_session_set_error_message(session, "NETCONF error occurred");
+    sr_session_set_error_message(session, "NETCONF error occurred.");
 
     /* error format */
     sr_session_set_error_format(session, "NETCONF");
