bf236f780c11097f5a76112985523b8352b0487b
Michal Vasko <mvasko@cesnet.cz>
2023-06-15 14:54:43 +0200

    shm sub UPDATE safer fake lock reacquire
    Make sure the lock does not end up in
    an inconsistent state having set flags
    for several lock types.

--- src/shm_sub.c
+++ src/shm_sub.c
@@ -481,8 +481,8 @@ _sr_shmsub_notify_wait_wr(sr_sub_shm_t *sub_shm, sr_sub_event_t event, uint32_t 
             }
         }
 
-        if (err_info && (err_info->err[0].err_code == SR_ERR_TIME_OUT)) {
-            /* UNLOCK mutex as well, on timeout caused by another lock we have lost the WRITE lock anyway */
+        if (sub_shm->lock.readers[0] || sub_shm->lock.writer) {
+            /* UNLOCK mutex, we do not really have the lock */
             sr_munlock(&sub_shm->lock.mutex);
         } else {
             /* set the WRITE lock back */
