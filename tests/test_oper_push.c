fde18583507910ddd6ae4cebbe6468524118e718
Michal Vasko <mvasko@cesnet.cz>
2023-09-18 09:07:12 +0200

    test REFACTOR formatting

--- tests/test_oper_push.c
+++ tests/test_oper_push.c
@@ -3146,7 +3146,7 @@ test_oper_set_del_leaflist(void **state)
     ret = sr_session_switch_ds(st->sess, SR_DS_OPERATIONAL);
     assert_int_equal(ret, SR_ERR_OK);
 
-    ret = sr_set_item_str(st->sess, xp_base , "", NULL, 0);
+    ret = sr_set_item_str(st->sess, xp_base, "", NULL, 0);
     assert_int_equal(ret, SR_ERR_OK);
 
     ret = sr_delete_item(st->sess, xp_attr, 0);
