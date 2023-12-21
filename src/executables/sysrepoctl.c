6dc81d0652cbfe95f4c1dad14a3d64eb3fc811d4
Michal Vasko <mvasko@cesnet.cz>
2023-12-21 15:03:08 +0100

    sysrepoctl UPDATE support for factory-default DS

--- src/executables/sysrepoctl.c
+++ src/executables/sysrepoctl.c
@@ -127,8 +127,8 @@ help_print(void)
             "                       if permissions are being changed.\n"
             "  -m, --module-plugin <mod-datastore>:<plugin-name>\n"
             "                       Set specific module datastore plugin for a module datastore (startup, running,\n"
-            "                       candidate, operational, or notification), can be specified multiple times for\n"
-            "                       different module datastores. Accepted by install op.\n"
+            "                       candidate, operational, factory-default, or notification), can be specified multiple\n"
+            "                       times for different module datastores. Accepted by install op.\n"
             "  -I, --init-data <path>\n"
             "                       Initial data in a file with XML or JSON extension to be set for module(s),\n"
             "                       useful when there are mandatory top-level nodes. Accepted by install op.\n"
@@ -278,6 +278,8 @@ set_datastore(const char *optarg, int *mod_ds)
         *mod_ds = SR_DS_CANDIDATE;
     } else if (!strcmp(optarg, "operational")) {
         *mod_ds = SR_DS_OPERATIONAL;
+    } else if (!strcmp(optarg, "factory-default")) {
+        *mod_ds = SR_DS_FACTORY_DEFAULT;
     } else if (!strcmp(optarg, "notification")) {
         *mod_ds = SR_MOD_DS_NOTIF;
     } else if (strcmp(optarg, ":ALL")) {
@@ -306,6 +308,8 @@ set_module_ds(const char *optarg, sr_module_ds_t *module_ds)
         module_ds_idx = SR_DS_CANDIDATE;
     } else if (!strncmp(optarg, "operational", ptr - optarg)) {
         module_ds_idx = SR_DS_OPERATIONAL;
+    } else if (!strncmp(optarg, "factory-default", ptr - optarg)) {
+        module_ds_idx = SR_DS_FACTORY_DEFAULT;
     } else if (!strncmp(optarg, "notification", ptr - optarg)) {
         module_ds_idx = SR_MOD_DS_NOTIF;
     } else {
