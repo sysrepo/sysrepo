25f01f0a6f98ed33e615b04caca155170eb297f1
Ondrej Kusnirik <Ondrej.Kusnirik@cesnet.cz>
2024-04-09 14:36:00 +0200

    test apply changes BUGFIX correct anyxml format

--- tests/test_apply_changes.c
+++ tests/test_apply_changes.c
@@ -2702,7 +2702,7 @@ apply_change_any_thread(void *arg)
     ret = sr_session_start(st->conn, SR_DS_RUNNING, &sess);
     assert_int_equal(ret, SR_ERR_OK);
 
-    ret = sr_set_item_str(sess, "/test:cont/anyx", "<some-xml><elem>value</elem></some-xml>", NULL, 0);
+    ret = sr_set_item_str(sess, "/test:cont/anyx", "<some-xml xmlns=\"urn:test\"><elem>value</elem></some-xml>", NULL, 0);
     assert_int_equal(ret, SR_ERR_OK);
     ret = sr_set_item_str(sess, "/test:cont/anyd", "{\"mod:some-data\": 24}", NULL, 0);
     assert_int_equal(ret, SR_ERR_OK);
