sysrepo/sysrepo

commit 1382043d6145ca8b08cc1e7c0655863345d64e37

nacm UPDATE explicit func for op checking

src/utils/nacm.c
@@ -1675,7 +1675,7 @@ cleanup:
 }
 
 sr_error_info_t *
-sr_nacm_check_operation(const char *nacm_user, const struct lyd_node *data, struct sr_denied *denied)
+sr_nacm_check_op(const char *nacm_user, const struct lyd_node *data, struct sr_denied *denied)
 {
     sr_error_info_t *err_info = NULL;
     const struct lyd_node *op = NULL;
@@ -1791,6 +1791,42 @@ cleanup:
 
     sr_nacm_free_groups(groups, group_count);
     return err_info;
+}
+
+API int
+sr_nacm_check_operation(sr_session_ctx_t *session, const struct lyd_node *op)
+{
+    sr_error_info_t *err_info = NULL;
+    struct sr_denied denied = {0};
+
+    SR_CHECK_ARG_APIRET(!session || !op, session, err_info);
+
+    if (!session->nacm_user) {
+        goto cleanup;
+    }
+
+    /* check the operation */
+    if ((err_info = sr_nacm_check_op(session->nacm_user, op, &denied))) {
+        goto cleanup;
+    }
+
+    if (denied.denied) {
+        /* access denied */
+        if (denied.rule_name) {
+            sr_log(SR_LL_ERR, "NACM access denied by the rule \"%s\".", denied.rule_name);
+        } else if (denied.def) {
+            sr_log(SR_LL_ERR, "NACM access denied by \"%s\" node extension \"%s\".", LYD_NAME(denied.node), denied.def->name);
+        } else {
+            sr_log(SR_LL_ERR, "NACM access denied by the default NACM permissions.");
+        }
+
+        sr_errinfo_new_nacm(&err_info, "protocol", "access-denied", NULL, denied.node,
+                "Executing the operation is denied because \"%s\" NACM authorization failed.", session->nacm_user);
+        goto cleanup;
+    }
+
+cleanup:
+    return sr_api_ret(session, err_info);
 }
 
 /**

src/shm_sub.c
@@ -4724,7 +4724,7 @@ sr_shmsub_notif_listen_process_module_events(struct modsub_notif_s *notif_subs, 
         /* check NACM */
         free(denied.rule_name);
         memset(&denied, 0, sizeof denied);
-        if (sub->sess->nacm_user && (err_info = sr_nacm_check_operation(sub->sess->nacm_user, notif, &denied))) {
+        if (sub->sess->nacm_user && (err_info = sr_nacm_check_op(sub->sess->nacm_user, notif, &denied))) {
             goto cleanup;
         }
 

src/sysrepo.c
@@ -7043,7 +7043,7 @@ sr_rpc_send_tree(sr_session_ctx_t *session, struct lyd_node *input, uint32_t tim
 
     if (session->nacm_user) {
         /* check NACM */
-        if ((err_info = sr_nacm_check_operation(session->nacm_user, input_top, &denied))) {
+        if ((err_info = sr_nacm_check_op(session->nacm_user, input_top, &denied))) {
             goto cleanup;
         }
 

src/sysrepo.h
@@ -4,8 +4,8 @@
  * @brief public API sysrepo header
  *
  * @copyright
- * Copyright (c) 2018 - 2022 Deutsche Telekom AG.
- * Copyright (c) 2018 - 2022 CESNET, z.s.p.o.
+ * Copyright (c) 2018 - 2025 Deutsche Telekom AG.
+ * Copyright (c) 2018 - 2025 CESNET, z.s.p.o.
  *
  * This source code is licensed under BSD 3-Clause License (the "License").
  * You may not use this file except in compliance with the License.

src/sysrepo_types.h
@@ -4,8 +4,8 @@
  * @brief public sysrepo types header
  *
  * @copyright
- * Copyright (c) 2018 - 2022 Deutsche Telekom AG.
- * Copyright (c) 2018 - 2022 CESNET, z.s.p.o.
+ * Copyright (c) 2018 - 2025 Deutsche Telekom AG.
+ * Copyright (c) 2018 - 2025 CESNET, z.s.p.o.
  *
  * This source code is licensed under BSD 3-Clause License (the "License").
  * You may not use this file except in compliance with the License.

src/utils/nacm.h
@@ -145,8 +145,7 @@ struct sr_denied {
  * @param[in,out] denied Deny details, if applicable.
  * @return err_info, NULL on success.
  */
-sr_error_info_t *sr_nacm_check_operation(const char *nacm_user, const struct lyd_node *data,
-        struct sr_denied *denied);
+sr_error_info_t *sr_nacm_check_op(const char *nacm_user, const struct lyd_node *data, struct sr_denied *denied);
 
 /**
  * @brief Filter out result nodes that do not have R access to.

src/utils/netconf_acm.h
@@ -4,8 +4,8 @@
  * @brief NACM functionality header
  *
  * @copyright
- * Copyright (c) 2019 - 2022 Deutsche Telekom AG.
- * Copyright (c) 2017 - 2022 CESNET, z.s.p.o.
+ * Copyright (c) 2019 - 2025 Deutsche Telekom AG.
+ * Copyright (c) 2017 - 2025 CESNET, z.s.p.o.
  *
  * This source code is licensed under BSD 3-Clause License (the "License").
  * You may not use this file except in compliance with the License.
@@ -85,6 +85,15 @@ const char *sr_nacm_get_user(sr_session_ctx_t *session);
  * @return Username of NACM recovery session.
  */
 const char *sr_nacm_get_recovery_user(void);
+
+/**
+ * @brief Explicitly check NACM access of an operation.
+ *
+ * @param[in] session Session to use, must have NACM username set for the check to be performed.
+ * @param[in] op Operation to check.
+ * @return Error code (::SR_ERR_OK on success).
+ */
+int sr_nacm_check_operation(sr_session_ctx_t *session, const struct lyd_node *op);
 
 #ifdef __cplusplus
 }

commit 573c0ebee3fbaa002f8f91a1fbc6b1f5709756c8

nacm BUGFIX memory leak
The rule_name string is copied and must be properly freed, otherwise
it causes a leak. I triggered it in sysrepo-cpp tests after wrapping
sr_nacm_check_operation function in sysrepo-cpp.
LSAN report follows:

  ==381000==ERROR: LeakSanitizer: detected memory leaks

  Direct leak of 2 byte(s) in 1 object(s) allocated from:
      #0 0x5d551e1cb56a in strdup (/build/sysrepo-cpp/build-clang-asan/test-session+0x1ae56a) (BuildId: d78ee32ce1fd55641090ef563448cfd2bd45b112)
      #1 0x7606a2a071f5 in sr_nacm_check_op /build/sysrepo/src/utils/nacm.c:1785:36
      #2 0x7606a2a0c796 in sr_nacm_check_operation /build/sysrepo/src/utils/nacm.c:1809:21
      #3 0x7606a382b0b0 in sysrepo::Session::checkNacmOperation(libyang::DataNode const&) const /build/sysrepo-cpp/src/Session.cpp:727:16
      #4 0x5d551e2428fc in DOCTEST_ANON_FUNC_2() /build/sysrepo-cpp/tests/session.cpp:459:9
      #5 0x5d551e2bbc8e in doctest::Context::run() /build/build/prefixes/clang-asan/include/doctest/doctest.h:7007:21
      #6 0x5d551e2c0702 in main /build/build/prefixes/clang-asan/include/doctest/doctest.h:7085:71
      #7 0x7606a2c35487 in __libc_start_call_main /usr/src/debug/glibc/glibc/csu/../sysdeps/nptl/libc_start_call_main.h:58:16
      #8 0x7606a2c3554b in __libc_start_main /usr/src/debug/glibc/glibc/csu/../csu/libc-start.c:360:3
      #9 0x5d551e0de564 in _start (/build/sysrepo-cpp/build-clang-asan/test-session+0xc1564) (BuildId: d78ee32ce1fd55641090ef563448cfd2bd45b112)

Fixes: 1382043d ("nacm UPDATE explicit func for op checking")

src/utils/nacm.c
@@ -1826,6 +1826,7 @@ sr_nacm_check_operation(sr_session_ctx_t *session, const struct lyd_node *op)
     }
 
 cleanup:
+    free(denied.rule_name);
     return sr_api_ret(session, err_info);
 }
 