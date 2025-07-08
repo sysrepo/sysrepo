c9382c4219bacc405559e3c69293dcc49f177223
roman <xjanot04@stud.fit.vutbr.cz>
2025-07-08 14:03:53 +0200

    sysrepo UPDATE cleanup after rebase

--- src/modinfo.h
+++ src/modinfo.h
@@ -69,19 +69,19 @@ struct sr_mod_info_s {
 };
 
 /**
- * @brief Initialize mod info structure and parse schema-mount data.
- *
- * @note The schema mount data are valid until ::sr_modinfo_erase() is called on the mod info.
+ * @brief Initialize mod info structure.
  *
  * @param[in,out] mod_info Mod info to initialize.
  * @param[in] conn Connection to use.
  * @param[in] ds Main datastore to use.
  * @param[in] ds2 Secondary datastore to use, if different from @p ds.
+ * @param[in] init_sm Whether to initialize schema-mount data. If set, the data will be valid
+ *                    until ::sr_modinfo_erase() is called on the mod info.
  * @param[in] op_id Operation ID of the operation, if 0 it is automatically incremented.
  * @return err_info, NULL on success.
  */
-sr_error_info_t *sr_modinfo_init_sm(struct sr_mod_info_s *mod_info, sr_conn_ctx_t *conn, sr_datastore_t ds, sr_datastore_t ds2,
-        uint32_t op_id);
+sr_error_info_t *sr_modinfo_init(struct sr_mod_info_s *mod_info, sr_conn_ctx_t *conn, sr_datastore_t ds, sr_datastore_t ds2,
+        int init_sm, uint32_t op_id);
 
 /**
  * @brief Add a new module and/or XPath into mod info.
