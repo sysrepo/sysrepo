0557d6ca2a691e0cbe6045ffd3cc0ea38aeb98b6
roman <xjanot04@stud.fit.vutbr.cz>
2025-07-30 10:15:21 +0200

    cmake UPDATE dont use functions in pctx module

--- CMakeModules/UsePrintedContext.cmake
+++ CMakeModules/UsePrintedContext.cmake
@@ -1,11 +1,7 @@
-# Configure printed context mapping for memory allocation.
-#
-# This module configures the printed context feature by determining the appropriate
-# memory address and checking system capabilities.
+# Configure printed libyang context address.
 #
 # The following variables are used:
 # PRINTED_CONTEXT_ADDRESS - Optional. Pre-defined address to use for the printed context mapping
-# ENABLE_PRINTED_CONTEXT - Optional. Enable/disable printed context feature (default: ON)
 #
 # The following cache variables are set:
 # PRINTED_CONTEXT_ADDRESS - The final address used for printed context mapping
@@ -19,8 +15,6 @@
 #    - Falls back to MAP_FIXED with potential undefined behavior
 # 5. Sets PRINTED_CONTEXT_ADDRESS for use in source code
 #
-# If address calculation fails and no manual address is provided, printed context is disabled.
-#
 # Author Roman Janota <janota@cesnet.cz>
 # Copyright (c) 2025 CESNET, z.s.p.o.
 #
@@ -31,43 +25,36 @@
 #     https://opensource.org/licenses/BSD-3-Clause
 #
 
-include(CheckCSourceCompiles)
+include(CheckSymbolExists)
 
-# clear any previous definitions
-set(CMAKE_REQUIRED_DEFINITIONS)
+# check if MAP_FIXED_NOREPLACE is available
+check_symbol_exists(MAP_FIXED_NOREPLACE "sys/mman.h" HAVE_MAP_FIXED_NOREPLACE)
 
-# set required definitions for checking MAP_FIXED_NOREPLACE
-set(CMAKE_REQUIRED_INCLUDES sys/mman.h)
-
-# check if MAP_FIXED_NOREPLACE is available by compiling a simple C program that uses it
-function(_check_map_fixed_noreplace)
-    check_c_source_compiles(
-        "
-        #include <sys/mman.h>
-
-        int main() {
-            int flag = MAP_FIXED_NOREPLACE;
-            (void)flag; // Suppress unused variable warning
-            return 0;
-        }
-        "
-        HAVE_MAP_FIXED_NOREPLACE
-    )
-
-    set(HAVE_MAP_FIXED_NOREPLACE ${HAVE_MAP_FIXED_NOREPLACE} PARENT_SCOPE)
-endfunction()
-
-# calculate printed context address
-function(_calculate_printed_context_address result_var)
-    set(${result_var} "" PARENT_SCOPE)
-
-    set(calculator_path "${CMAKE_CURRENT_LIST_DIR}/pctx_addr_calculator.c")
-
-    # check if calculator source exists
-    if(NOT EXISTS "${calculator_path}")
-        message(WARNING "Printed context address calculator not found: ${calculator_path}")
+# handle case when MAP_FIXED_NOREPLACE is not available
+if(NOT HAVE_MAP_FIXED_NOREPLACE)
+    # MAP_FIXED_NOREPLACE is not available, but printed context may still be used with MAP_FIXED
+    if(DEFINED PRINTED_CONTEXT_ADDRESS)
+        message(WARNING "MAP_FIXED_NOREPLACE not available, but printed context requested. "
+                        "Consider disabling ASLR to avoid undefined behavior.")
+    else()
+        # no address provided, printed context cannot be used, we can end here
+        message(WARNING "MAP_FIXED_NOREPLACE not available and no address provided. "
+                        "Provide page-aligned PRINTED_CONTEXT_ADDRESS and consider disabling ASLR to use printed context. "
+                        "Printed context disabled.")
         return()
     endif()
+endif()
+
+# determine the address to use
+set(address "")
+
+if(DEFINED PRINTED_CONTEXT_ADDRESS)
+    set(address ${PRINTED_CONTEXT_ADDRESS})
+else()
+    message(STATUS "Calculating printed context address...")
+
+    # calculate printed context address
+    set(calculator_path "${CMAKE_CURRENT_LIST_DIR}/pctx_addr_calculator.c")
 
     # compile the calculator
     try_compile(calc_success
@@ -79,81 +66,29 @@ function(_calculate_printed_context_address result_var)
 
     if(NOT calc_success)
         message(WARNING "Failed to compile address calculator:\n${compile_output}")
-        return()
-    endif()
-
-    # execute the address calculator
-    execute_process(
-        COMMAND ${CMAKE_BINARY_DIR}/pctx_addr_calculator
-        OUTPUT_VARIABLE calculated_addr
-        OUTPUT_STRIP_TRAILING_WHITESPACE
-        RESULT_VARIABLE exec_result
-        ERROR_VARIABLE exec_error
-    )
-
-    if(NOT exec_result EQUAL 0)
-        message(WARNING "Address calculation failed (${exec_result}): ${exec_error}")
-        return()
-    endif()
-
-    # propagate the calculated address to the calling scope
-    set(${result_var} ${calculated_addr} PARENT_SCOPE)
-endfunction()
-
-# configure printed context
-function(_configure_printed_context)
-    # determine the address to use
-    if(DEFINED PRINTED_CONTEXT_ADDRESS)
-        set(address ${PRINTED_CONTEXT_ADDRESS})
-        message(STATUS "Using provided printed context address: ${address}")
     else()
-        message(STATUS "Calculating printed context address...")
+        # execute the address calculator
+        execute_process(
+            COMMAND ${CMAKE_BINARY_DIR}/pctx_addr_calculator
+            OUTPUT_VARIABLE calculated_addr
+            OUTPUT_STRIP_TRAILING_WHITESPACE
+            RESULT_VARIABLE exec_result
+            ERROR_VARIABLE exec_error
+        )
 
-        # this will set address variable to the calculated address on success
-        _calculate_printed_context_address(address)
-
-        if(NOT address)
-            message(WARNING "Could not determine printed context address. "
-                           "Provide PRINTED_CONTEXT_ADDRESS manually or set ENABLE_PRINTED_CONTEXT=OFF")
-            return()
-        endif()
-
-        message(STATUS "Using calculated printed context address: ${address}")
-    endif()
-
-    # set and cache the address for use in source code
-    set(PRINTED_CONTEXT_ADDRESS ${address} CACHE INTERNAL "Printed context address")
-endfunction()
-
-# check if MAP_FIXED_NOREPLACE is available, this will set HAVE_MAP_FIXED_NOREPLACE variable
-_check_map_fixed_noreplace()
-
-# MAP_FIXED_NOREPLACE is not available, but printed context may still be used with MAP_FIXED
-if(NOT HAVE_MAP_FIXED_NOREPLACE)
-    if(ENABLE_PRINTED_CONTEXT)
-        if(DEFINED PRINTED_CONTEXT_ADDRESS)
-            # use printed context with provided address
-            message(WARNING "MAP_FIXED_NOREPLACE not available but printed context requested. "
-                           "Using provided address ${PRINTED_CONTEXT_ADDRESS}. "
-                           "Consider disabling ASLR to avoid undefined behavior.")
-            _configure_printed_context()
+        if(NOT exec_result EQUAL 0)
+            message(WARNING "Address calculation failed (${exec_result}): ${exec_error}")
         else()
-            # no address provided, printed context cannot be used
-            message(WARNING "MAP_FIXED_NOREPLACE not available and no address provided. "
-                           "Provide page-aligned PRINTED_CONTEXT_ADDRESS and disable ASLR, or set ENABLE_PRINTED_CONTEXT=OFF. "
-                           "Printed context disabled.")
+            set(address ${calculated_addr})
         endif()
-    else()
-        message(STATUS "MAP_FIXED_NOREPLACE not available. Printed context disabled.")
     endif()
-    return()
 endif()
 
-# MAP_FIXED_NOREPLACE is available
-if((NOT DEFINED ENABLE_PRINTED_CONTEXT) OR ENABLE_PRINTED_CONTEXT)
-    # this module is included before options in main Cmake are set, but
-    # ENABLE_PRINTED_CONTEXT should be ON by default so just assume it is ON if it is undefined
-    _configure_printed_context()
+# set and cache the address for use in source code
+if (NOT address)
+    message(WARNING "Could not determine printed context address. "
+                       "Provide PRINTED_CONTEXT_ADDRESS manually to enable printed context.")
 else()
-    message(STATUS "Printed context disabled.")
+    set(PRINTED_CONTEXT_ADDRESS ${address} CACHE INTERNAL "Printed context address")
+    message(STATUS "Using printed context address: ${PRINTED_CONTEXT_ADDRESS}")
 endif()
