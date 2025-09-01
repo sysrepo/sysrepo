1cab564abccdfc5c66081288e2c95f96cf3e0b5b
Michal Vasko <mvasko@cesnet.cz>
2025-09-01 09:27:54 +0200

    modinfo REFACTOR formatting

--- src/modinfo.c
+++ src/modinfo.c
@@ -4132,8 +4132,8 @@ sr_modinfo_data_store(struct sr_mod_info_s *mod_info, sr_session_ctx_t *session,
 
                 if (ATOMIC_LOAD_RELAXED(sr_run_cache.enabled)) {
                     /* store the changed data in the cache */
-                    if ((err_info = sr_run_cache_update_mod(session->conn, &sr_run_cache, mod->ly_mod, mod->shm_mod->run_cache_id,
-                            mod_data))) {
+                    if ((err_info = sr_run_cache_update_mod(mod_info->conn, &sr_run_cache, mod->ly_mod,
+                            mod->shm_mod->run_cache_id, mod_data))) {
                         /* not a fatal error, cache can be updated in a future operation */
                         sr_errinfo_free(&err_info);
                         lyd_free_siblings(mod_data);
