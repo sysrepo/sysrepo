70ba3f56f1c22207992bcf5f85fb25d986da7f87
Roman Janota <Roman.Janota@cesnet.cz>
2026-08-06 15:57:14 +0200

    scripts BUGFIX setup_repo_shm.sh correct user

--- scripts/setup_repo_shm.sh
+++ scripts/setup_repo_shm.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env bash
 
+# When invoked through sudo, $USER contains the target user (root).
+# The original caller is in $SUDO_USER. Prefer it so we drop back to that user.
+if [ -n "$SUDO_USER" ]; then
+    RUN_USER="$SUDO_USER"
+else
+    RUN_USER="$USER"
+fi
+
 # get path to sysrepoctl executable, this will be stored in $SYSREPOCTL
 if [ -n "$SYSREPOCTL_EXECUTABLE" ]; then
     SYSREPOCTL="$SYSREPOCTL_EXECUTABLE"
-elif [ $(id -u) -eq 0 ] && [ -n "$USER" ] && [ $(command -v su) ]; then
-    SYSREPOCTL=$(su -c 'command -v sysrepoctl' -l "$USER")
+elif [ $(id -u) -eq 0 ] && [ -n "$RUN_USER" ] && [ $(command -v su) ]; then
+    SYSREPOCTL=$(su -c 'command -v sysrepoctl' -l "$RUN_USER")
 else
     SYSREPOCTL=$(command -v sysrepoctl)
 fi
@@ -15,8 +23,8 @@ if [ -z "$SYSREPOCTL" ]; then
 fi
 
 # run sysrepoctl so that a connection is created and repository and SHM initialized (ideally as a non-privileged user)
-if [ $(id -u) -eq 0 ] && [ -n "$USER" ] && [ $(command -v su) ]; then
-    su -c "$SYSREPOCTL -L" -l $USER
+if [ $(id -u) -eq 0 ] && [ -n "$RUN_USER" ] && [ $(command -v su) ]; then
+    su -c "$SYSREPOCTL -L" -l "$RUN_USER"
 else
     "$SYSREPOCTL" -L > /dev/null
 fi
